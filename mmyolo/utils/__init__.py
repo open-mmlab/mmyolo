5242996dd10c1dfab2db5fb540bb7c3d30122a23
hha <1286304229@qq.com>
2022-09-18 14:04:11 +0800

    Add utils

---
+++ mmyolo/utils/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .collect_env import collect_env
+from .setup_env import register_all_modules
+
+__all__ = ['register_all_modules', 'collect_env']
