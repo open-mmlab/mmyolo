d326550b2b1716388507781d9cbad25d99cb6af5
wanghonglie <wanghonglie@pjlab.org.cn>
2022-09-18 13:05:32 +0800

    add engine (#18)

---
+++ mmyolo/engine/hooks/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .yolov5_param_scheduler_hook import YOLOv5ParamSchedulerHook
+from .yolox_mode_switch_hook import YOLOXModeSwitchHook
+
+__all__ = ['YOLOv5ParamSchedulerHook', 'YOLOXModeSwitchHook']
