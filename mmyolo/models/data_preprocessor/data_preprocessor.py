8ce098717f0f931b81a7cfc50f1ac38a37a520b4
HinGwenWoong <peterhuang0323@qq.com>
2022-09-18 13:06:09 +0800

    [Feature] Add data preprocessor (#19)

---
+++ mmyolo/models/data_preprocessor/data_preprocessor.py
@@ -0,0 +1,42 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+from mmdet.models.data_preprocessors import DetDataPreprocessor
+
+from mmyolo.registry import MODELS
+
+
+@MODELS.register_module()
+class YOLOv5DetDataPreprocessor(DetDataPreprocessor):
+    """Rewrite collate_fn to get faster training speed.
+
+    Note: It must be used together with `mmyolo.datasets.utils.yolov5_collate`
+    """
+
+    def forward(self, data: dict, training: bool = False) -> dict:
+        if not training:
+            return super().forward(data, training)
+        assert isinstance(data['data_samples'], torch.Tensor), \
+            '"data_samples" should be a tensor, but got ' \
+            f'{type(data["data_samples"])}. The possible reason for this ' \
+            'is that you are not using it with ' \
+            '"mmyolo.datasets.utils.yolov5_collate". Please refer to ' \
+            '"configs/yolov5/yolov5_s-v61_syncbn_fast_8xb16-300e_coco.py".'
+
+        inputs = data['inputs'].to(self.device, non_blocking=True)
+
+        if self._channel_conversion and inputs.shape[1] == 3:
+            inputs = inputs[:, [2, 1, 0], ...]
+
+        if self._enable_normalize:
+            inputs = (inputs - self.mean) / self.std
+
+        data_samples = data['data_samples'].to(self.device, non_blocking=True)
+
+        if self.batch_augments is not None:
+            for batch_aug in self.batch_augments:
+                inputs, data_samples = batch_aug(inputs, data_samples)
+
+        img_metas = [{'batch_input_shape': inputs.shape[2:]}] * len(inputs)
+        data_samples = {'bboxes_labels': data_samples, 'img_metas': img_metas}
+
+        return {'inputs': inputs, 'data_samples': data_samples}
