3167c469693b5812cb6e49aa456bdd83dadb24e0
Nioolek <40284075+Nioolek@users.noreply.github.com>
2022-09-18 12:17:29 +0800

    Add yolov5 head and doc (#14)
    * add yolov5_description.md
    
    * add yolov5_head.py

---
+++ mmyolo/models/dense_heads/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .yolov5_head import YOLOv5Head, YOLOv5HeadModule
+from .yolov6_head import YOLOv6Head, YOLOv6HeadModule
+from .yolox_head import YOLOXHead, YOLOXHeadModule
+
+__all__ = [
+    'YOLOv5Head', 'YOLOv6Head', 'YOLOXHead', 'YOLOv5HeadModule',
+    'YOLOv6HeadModule', 'YOLOXHeadModule'
+]
