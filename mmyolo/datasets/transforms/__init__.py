94111555d671b9eed62b9568f5bb6b72b85b4dd4
HinGwenWoong <peterhuang0323@qq.com>
2022-09-18 10:51:50 +0800

    Add dataset mixing transforms (#5)

--- mmyolo/datasets/transforms/__init__.py
+++ mmyolo/datasets/transforms/__init__.py
@@ -1,8 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .mix_img_transforms import Mosaic, YOLOv5MixUp, YOLOXMixUp
 from .transforms import (LetterResize, LoadAnnotations, YOLOv5HSVRandomAug,
                          YOLOv5KeepRatioResize, YOLOv5RandomAffine)
 
 __all__ = [
-    'YOLOv5KeepRatioResize', 'LetterResize', 'YOLOv5HSVRandomAug', 'LoadAnnotations',
+    'YOLOv5KeepRatioResize', 'LetterResize', 'Mosaic', 'YOLOXMixUp',
+    'YOLOv5MixUp', 'YOLOv5HSVRandomAug', 'LoadAnnotations',
     'YOLOv5RandomAffine'
 ]
