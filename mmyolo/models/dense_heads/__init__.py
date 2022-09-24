0b2d30a9a6cee5b96c4cbb28972a51326af55f6c
hha <1286304229@qq.com>
2022-09-24 13:53:49 +0800

    add head

--- mmyolo/models/dense_heads/__init__.py
+++ mmyolo/models/dense_heads/__init__.py
@@ -2,8 +2,9 @@
 from .yolov5_head import YOLOv5Head, YOLOv5HeadModule
 from .yolov6_head import YOLOv6Head, YOLOv6HeadModule
 from .yolox_head import YOLOXHead, YOLOXHeadModule
+from .rtmdet_head import RTMDetHead, RTMDetHeadModule
 
 __all__ = [
     'YOLOv5Head', 'YOLOv6Head', 'YOLOXHead', 'YOLOv5HeadModule',
-    'YOLOv6HeadModule', 'YOLOXHeadModule'
+    'YOLOv6HeadModule', 'YOLOXHeadModule', 'RTMDetHead', 'RTMDetHeadModule'
 ]
