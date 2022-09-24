d2081c4e849b8b608f64edb0118dc3525cdcd8b3
hha <1286304229@qq.com>
2022-09-24 11:43:22 +0800

    add neck

--- mmyolo/models/necks/__init__.py
+++ mmyolo/models/necks/__init__.py
@@ -3,5 +3,6 @@ from .base_yolo_neck import BaseYOLONeck
 from .yolov5_pafpn import YOLOv5PAFPN
 from .yolov6_pafpn import YOLOv6RepPAFPN
 from .yolox_pafpn import YOLOXPAFPN
+from .cspnext_pafpn import CSPNeXtPAFPN
 
-__all__ = ['YOLOv5PAFPN', 'BaseYOLONeck', 'YOLOv6RepPAFPN', 'YOLOXPAFPN']
+__all__ = ['YOLOv5PAFPN', 'BaseYOLONeck', 'YOLOv6RepPAFPN', 'YOLOXPAFPN', 'CSPNeXtPAFPN']
