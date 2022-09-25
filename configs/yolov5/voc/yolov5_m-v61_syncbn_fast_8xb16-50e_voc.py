272ae733733825c2d72492fe43595fc799d8d976
gulingrui <379319054@qq.com>
2022-09-25 22:15:59 +0800

    fix mosaic bug and temp config

--- configs/yolov5/voc/yolov5_m-v61_syncbn_fast_8xb16-50e_voc.py
+++ configs/yolov5/voc/yolov5_m-v61_syncbn_fast_8xb16-50e_voc.py
@@ -2,22 +2,78 @@ _base_ = './yolov5_s-v61_syncbn_fast_8xb16-50e_voc.py'
 
 deepen_factor = 0.67
 widen_factor = 0.75
-lr_factor = 0.1  # lrf=0.1
-affine_scale = 0.9
+max_epochs = 50
+lr_factor = 0.15135  # lrf=0.1
+affine_scale = 0.75544
+train_batch_size_per_gpu = 32
+
+anchors = [[(10, 13), (16, 30), (33, 23)], [(30, 61), (62, 45), (59, 119)],
+           [(116, 90), (156, 198), (373, 326)]]
+strides = [8, 16, 32]
 
 model = dict(
+    type='YOLODetector',
+    data_preprocessor=dict(
+        type='mmdet.DetDataPreprocessor',
+        mean=[0., 0., 0.],
+        std=[255., 255., 255.],
+        bgr_to_rgb=True),
     backbone=dict(
+        type='YOLOv5CSPDarknet',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
-    ),
+        norm_cfg=dict(type='BN', momentum=0.03, eps=0.001),
+        act_cfg=dict(type='SiLU', inplace=True)),
     neck=dict(
+        type='YOLOv5PAFPN',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
-    ),
+        in_channels=[256, 512, 1024],
+        out_channels=[256, 512, 1024],
+        num_csp_blocks=3,
+        norm_cfg=dict(type='BN', momentum=0.03, eps=0.001),
+        act_cfg=dict(type='SiLU', inplace=True)),
     bbox_head=dict(
-        head_module=dict(widen_factor=widen_factor),
-        loss_cls=dict(loss_weight=0.3),
-        loss_obj=dict(loss_weight=0.7)))
+        type='YOLOv5Head',
+        head_module=dict(
+            type='YOLOv5HeadModule',
+            num_classes=20,
+            in_channels=[256, 512, 1024],
+            widen_factor=widen_factor,
+            featmap_strides=strides,
+            num_base_priors=3),
+        prior_generator=dict(
+            type='mmdet.YOLOAnchorGenerator',
+            base_sizes=anchors,
+            strides=strides),
+        loss_cls=dict(
+            type='mmdet.CrossEntropyLoss',
+            use_sigmoid=True,
+            reduction='mean',
+            loss_weight=0.21638,
+            class_weight=0.5),
+        loss_bbox=dict(
+            type='IoULoss',
+            iou_mode='ciou',
+            bbox_format='xywh',
+            eps=1e-7,
+            reduction='mean',
+            loss_weight=0.02,
+            return_iou=True),
+        loss_obj=dict(
+            type='mmdet.CrossEntropyLoss',
+            use_sigmoid=True,
+            reduction='mean',
+            loss_weight=0.51728,
+            class_weight=0.67198),
+        prior_match_thr=3.3744,
+        obj_level_weights=[4., 1., 0.4]),
+    test_cfg=dict(
+        multi_label=True,
+        nms_pre=30000,
+        score_thr=0.001,
+        nms=dict(type='nms', iou_threshold=0.65),
+        max_per_img=300))
 
 pre_transform = _base_.pre_transform
 albu_train_transforms = _base_.albu_train_transforms
@@ -28,10 +84,12 @@ mosaic_affine_pipeline = [
         type='Mosaic',
         img_scale=img_scale,
         pad_val=114.0,
-        pre_transform=pre_transform),
+        pre_transform=pre_transform,
+        prob=0.85834),
     dict(
         type='YOLOv5RandomAffine',
         max_rotate_degree=0.0,
+        max_translate_ratio=0.04591,
         max_shear_degree=0.0,
         scaling_ratio_range=(1 - affine_scale, 1 + affine_scale),
         border=(-img_scale[0] // 2, -img_scale[1] // 2),
@@ -43,7 +101,7 @@ train_pipeline = [
     *pre_transform, *mosaic_affine_pipeline,
     dict(
         type='YOLOv5MixUp',
-        prob=0.1,
+        prob=0.04266,
         pre_transform=[*pre_transform, *mosaic_affine_pipeline]),
     dict(
         type='mmdet.Albu',
@@ -56,7 +114,10 @@ train_pipeline = [
             'img': 'image',
             'gt_bboxes': 'bboxes'
         }),
-    dict(type='YOLOv5HSVRandomAug'),
+    dict(type='YOLOv5HSVRandomAug',
+         hue_delta=0.01041,
+         saturation_delta=0.54703,
+         value_delta=0.27739),
     dict(type='mmdet.RandomFlip', prob=0.5),
     dict(
         type='mmdet.PackDetInputs',
@@ -85,4 +146,27 @@ train_dataloader = dict(
         ])
 )
 
-default_hooks = dict(param_scheduler=dict(lr_factor=lr_factor))
+param_scheduler = None
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='SGD',
+        lr=0.01,
+        momentum=0.937,
+        weight_decay=0.0005,
+        nesterov=True,
+        batch_size_per_gpu=train_batch_size_per_gpu),
+    constructor='YOLOv5OptimizerConstructor')
+
+default_hooks = dict(
+    param_scheduler=dict(
+        type='YOLOv5ParamSchedulerHook',
+        scheduler_type='linear',
+        lr_factor=lr_factor,
+        max_epochs=max_epochs,
+        warmup_epochs=3.3835,
+        warmup_momentum=0.59462,
+        warmup_bias_lr=0.18657),
+    checkpoint=dict(
+        # type='CheckpointHook', interval=save_epoch_intervals,
+        max_keep_ckpts=3))
