f6ebf122ec09befac5d7f40b159fa2cb59f4a1b3
hanrui1sensetime <hanrui1@sensetime.com>
2022-09-26 14:08:28 +0800

    remove unused initialize

--- mmyolo/deploy/__init__.py
+++ mmyolo/deploy/__init__.py
@@ -1,16 +1,2 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import importlib
-from mmdeploy.utils import get_root_logger
 from mmdeploy.version import __version__  # noqa F401
-
-if importlib.util.find_spec('torch'):
-    importlib.import_module('mmdeploy.pytorch')
-else:
-    logger = get_root_logger()
-    logger.debug('torch is not installed.')
-
-if importlib.util.find_spec('mmcv'):
-    importlib.import_module('mmdeploy.mmcv')
-else:
-    logger = get_root_logger()
-    logger.debug('mmcv is not installed.')
