a33d612583acfbc5d570528162283fbd1c72510f
hanrui1sensetime <hanrui1@sensetime.com>
2022-09-27 16:47:07 +0800

    fix comment

--- mmyolo/deploy/__init__.py
+++ mmyolo/deploy/__init__.py
@@ -1,10 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmdeploy.codebase.base import BaseTask, MMCodebase, get_codebase_class
+from mmdeploy.codebase.base import MMCodebase
 
 from .models import *  # noqa: F401,F403
 from .object_detection import MMYOLO, YOLOObjectDetection
 
 __all__ = [
-    'MMCodebase', 'BaseTask', 'get_codebase_class', 'MMYOLO',
-    'YOLOObjectDetection'
+    'MMCodebase', 'MMYOLO', 'YOLOObjectDetection'
 ]
