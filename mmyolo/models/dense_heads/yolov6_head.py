135667c867bbd3781e3117492eb6b7c69de1fb75
wanghonglie <wanghonglie@pjlab.org.cn>
2022-10-11 16:03:26 +0800

    fix detach bug

--- mmyolo/models/dense_heads/yolov6_head.py
+++ mmyolo/models/dense_heads/yolov6_head.py
@@ -354,7 +354,7 @@ class YOLOv6Head(YOLOv5Head):
         # cls loss
         target_labels = torch.where(fg_mask > 0, target_labels, torch.full_like(target_labels, self.num_classes))
         one_hot_label = F.one_hot(target_labels.long(), self.num_classes + 1)[..., :-1]
-        pred_scores = torch.sigmoid(flatten_cls_preds.detach())
+        pred_scores = torch.sigmoid(flatten_cls_preds)
         loss_cls = self.varifocal_loss(pred_scores, target_scores, one_hot_label)
         # weighted_target = one_hot_label * target_scores
         # loss_cls = self.loss_cls(flatten_cls_preds.view(-1,self.num_classes), (target_scores*one_hot_label).view(-1,self.num_classes))
