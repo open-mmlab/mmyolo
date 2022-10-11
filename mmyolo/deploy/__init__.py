77d7ca22789c85415589b7ae3d34b3a0f9542a25
hanrui1sensetime <hanrui1@sensetime.com>
2022-10-11 20:11:53 +0800

    fix yapf

--- mmyolo/deploy/__init__.py
+++ mmyolo/deploy/__init__.py
@@ -4,6 +4,4 @@ from mmdeploy.codebase.base import MMCodebase
 from .models import *  # noqa: F401,F403
 from .object_detection import MMYOLO, YOLOObjectDetection
 
-__all__ = [
-    'MMCodebase', 'MMYOLO', 'YOLOObjectDetection'
-]
+__all__ = ['MMCodebase', 'MMYOLO', 'YOLOObjectDetection']
