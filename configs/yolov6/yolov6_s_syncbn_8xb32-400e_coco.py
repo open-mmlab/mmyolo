019bec8194467b403d0e2d9664af3791c9581e07
wanghonglie <wanghonglie@pjlab.org.cn>
2022-10-12 14:52:45 +0800

    update

--- configs/yolov6/yolov6_s_syncbn_8xb32-400e_coco.py
+++ configs/yolov6/yolov6_s_syncbn_8xb32-400e_coco.py
@@ -1,24 +1,51 @@
-# Training mode is currently not supported
+_base_ = '../_base_/default_runtime.py'
 
-_base_ = '../yolov5/yolov5_s-v61_syncbn_8xb16-300e_coco.py'
-max_epochs = 400
-train_batch_size_per_gpu = 32
-img_scale = (640, 640)  # height, width
+# dataset settings
+data_root = 'data/coco100/'
+dataset_type = 'YOLOv5CocoDataset'
+
 num_last_epochs = 15
-save_epoch_intervals = 10
 
-deepen_factor = _base_.deepen_factor
-widen_factor = _base_.widen_factor
+# parameters that often need to be modified
+img_scale = (640, 640)  # height, width
+deepen_factor = 0.33
+widen_factor = 0.5
+max_epochs = 400
+save_epoch_intervals = 10
+train_batch_size_per_gpu = 4
+train_num_workers = 8
+val_batch_size_per_gpu = 1
+val_num_workers = 2
+
+# persistent_workers must be False if num_workers is 0.
+persistent_workers = True
+
+# only on Val
+batch_shapes_cfg = dict(
+    type='BatchShapePolicy',
+    batch_size=val_batch_size_per_gpu,
+    img_size=img_scale[0],
+    size_divisor=32,
+    extra_pad_ratio=0.5)
+
+# single-scale training is recommended to
+# be turned on, which can speed up training.
+env_cfg = dict(cudnn_benchmark=True)
+
 model = dict(
+    type='YOLODetector',
+    data_preprocessor=dict(
+        type='mmdet.DetDataPreprocessor',
+        mean=[0., 0., 0.],
+        std=[255., 255., 255.],
+        bgr_to_rgb=True),
     backbone=dict(
-        _delete_=True,
         type='YOLOv6EfficientRep',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
         norm_cfg=dict(type='BN', momentum=0.03, eps=0.001),
         act_cfg=dict(type='ReLU', inplace=True)),
     neck=dict(
-        _delete_=True,
         type='YOLOv6RepPAFPN',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
@@ -29,7 +56,6 @@ model = dict(
         act_cfg=dict(type='ReLU', inplace=True),
     ),
     bbox_head=dict(
-        _delete_=True,
         type='YOLOv6Head',
         head_module=dict(
             type='YOLOv6HeadModule',
@@ -43,7 +69,13 @@ model = dict(
         initial_epoch=4,
         initial_assigner=dict(type='BatchATSSAssigner', topk=9, iou2d_calculator=dict(type='mmdet.BboxOverlaps2D'), num_classes=80),
         assigner=dict(type='BatchTaskAlignedAssigner', topk=13, alpha=1, beta=6),
-        ))
+        ),
+    test_cfg=dict(
+        multi_label=True,
+        nms_pre=30000,
+        score_thr=0.001,
+        nms=dict(type='nms', iou_threshold=0.65),
+        max_per_img=300))
 
 pre_transform = [
     dict(
@@ -83,6 +115,66 @@ train_pipeline_stage2 = [
                    'flip_direction'))
 ]
 
+train_dataloader = dict(
+    batch_size=train_batch_size_per_gpu,
+    num_workers=train_num_workers,
+    persistent_workers=persistent_workers,
+    pin_memory=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='annotations/instances_train2017.json',
+        data_prefix=dict(img='train2017/'),
+        filter_cfg=dict(filter_empty_gt=False, min_size=32),
+        pipeline=train_pipeline))
+
+test_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        file_client_args={{_base_.file_client_args}}),
+    dict(type='YOLOv5KeepRatioResize', scale=img_scale),
+    dict(
+        type='LetterResize',
+        scale=img_scale,
+        allow_scale_up=False,
+        pad_val=dict(img=114)),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='mmdet.PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor', 'pad_param'))
+]
+
+val_dataloader = dict(
+    batch_size=val_batch_size_per_gpu,
+    num_workers=val_num_workers,
+    persistent_workers=persistent_workers,
+    pin_memory=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        test_mode=True,
+        data_prefix=dict(img='val2017/'),
+        ann_file='annotations/instances_val2017.json',
+        pipeline=test_pipeline,
+        batch_shapes_cfg=batch_shapes_cfg))
+
+test_dataloader = val_dataloader
+
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='SGD',
+        lr=0.01,
+        momentum=0.937,
+        weight_decay=0.0005,
+        nesterov=True,
+        batch_size_per_gpu=train_batch_size_per_gpu),
+    constructor='YOLOv5OptimizerConstructor')
+
 default_hooks = dict(
     param_scheduler=dict(
         type='YOLOv5ParamSchedulerHook',
@@ -108,8 +200,17 @@ custom_hooks = [
         switch_pipeline=train_pipeline_stage2)
 ]
 
+val_evaluator = dict(
+    type='mmdet.CocoMetric',
+    proposal_nums=(100, 1, 10),
+    ann_file=data_root + 'annotations/instances_val2017.json',
+    metric='bbox')
+test_evaluator = val_evaluator
+
 train_cfg = dict(
     type='EpochBasedTrainLoop',
     max_epochs=max_epochs,
     val_interval=save_epoch_intervals,
     dynamic_intervals=[(max_epochs - num_last_epochs, 1)])
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
