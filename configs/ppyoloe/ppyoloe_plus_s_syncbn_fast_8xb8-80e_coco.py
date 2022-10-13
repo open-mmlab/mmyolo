ea567eadbe17f88cf4c03c4ab0adee578e76e0e7
gulingrui <379319054@qq.com>
2022-10-13 17:06:11 +0800

    rename config

--- configs/ppyoloe/ppyoloe_plus_s_syncbn_fast_8xb8-80e_coco.py
+++
@@ -1,7 +0,0 @@
-_base_ = './ppyoloe_s_syncbn_fast_8xb16-300e_coco.py'
-
-train_batch_size_per_gpu = 8
-
-model = dict(
-    data_preprocessor=dict(mean=[0., 0., 0.], std=[255., 255., 255.]),
-    backbone=dict(use_alpha=True))
