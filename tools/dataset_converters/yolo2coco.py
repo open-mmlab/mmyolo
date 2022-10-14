58fa514d62531c1e0e7860aa3fc74b163029ad68
xin-li-67 <williamlee.xin@gmail.com>
2022-10-14 16:07:29 +0800

    encaped and modified yolo2coco

--- tools/dataset_converters/yolo2coco.py
+++ tools/dataset_converters/yolo2coco.py
@@ -6,7 +6,60 @@ import mmcv
 import mmengine
 
 
-def format_by_file(image_dir: str):
+def check_existance(file_path: str):
+    """Check if target file is existed."""
+    if not osp.exists(file_path):
+        raise FileNotFoundError(f'{file_path} does not exist!')
+
+
+def get_image_info(yolo_image_dir, idx, file_name):
+    """Retrieve image information."""
+    img_path = osp.join(yolo_image_dir, file_name)
+    check_existance(img_path)
+
+    img = mmcv.imread(img_path)
+    height, width = img.shape[:2]
+    img_info_dict = {
+        'file_name': file_name,
+        'id': idx,
+        'width': width,
+        'height': height
+    }
+
+    return img_info_dict, height, width
+
+
+def convert_bbox_info(label, idx, obj_count, H, W):
+    """Convert yolo-style bbox info to the coco format."""
+    label = label.strip().split()
+    x = float(label[1])
+    y = float(label[2])
+    w = float(label[3])
+    h = float(label[4])
+
+    # convert x,y,w,h to x1,y1,x2,y2
+    x1 = (x - w / 2) * W
+    y1 = (y - h / 2) * H
+    x2 = (x + w / 2) * W
+    y2 = (y + h / 2) * H
+
+    cls_id = int(label[0])
+    width = max(0., x2 - x1)
+    height = max(0., y2 - y1)
+    coco_format_info = {
+        'image_id': idx,
+        'id': obj_count,
+        'category_id': cls_id,
+        'bbox': [x1, y1, width, height],
+        'area': width * height,
+        'segmentation': [[x1, y1, x2, y1, x2, y2, x1, y2]],
+        'iscrowd': 0
+    }
+
+    return coco_format_info, obj_count
+
+
+def organize_by_existing_files(image_dir: str):
     """Format annotations by existing train/val/test files."""
     categories = ['train', 'val', 'test']
     image_list = []
@@ -32,20 +85,22 @@ def convert_yolo_to_coco(image_dir: str, split: bool = False):
             train.txt, val.txt, and test.txt
     """
     print(f'Start to load existing images and annotations from {image_dir}')
-    assert osp.exists(image_dir)
+    check_existance(image_dir)
 
     yolo_label_dir = osp.join(image_dir, 'labels')
     yolo_image_dir = osp.join(image_dir, 'images')
     yolo_class_txt = osp.join(image_dir, 'classes.txt')
+    check_existance(yolo_label_dir)
+    check_existance(yolo_image_dir)
+    check_existance(yolo_class_txt)
+
     with open(yolo_class_txt) as f:
         classes = f.read().strip().split()
 
     indices = os.listdir(yolo_image_dir)
 
     if split:
-        print(
-            'Start to organize data according to existing train/test/val split'
-        )
+        print('Start to work based to existing train, test, and val')
         train_dataset = {'images': [], 'annotations': [], 'categories': []}
         val_dataset = {'images': [], 'annotations': [], 'categories': []}
         test_dataset = {'images': [], 'annotations': [], 'categories': []}
@@ -55,86 +110,53 @@ def convert_yolo_to_coco(image_dir: str, split: bool = False):
             train_dataset['categories'].append({'id': i, 'name': cls})
             val_dataset['categories'].append({'id': i, 'name': cls})
             test_dataset['categories'].append({'id': i, 'name': cls})
-        train_img, val_img, test_img = format_by_file(image_dir)
+        train_img, val_img, test_img = organize_by_existing_files(image_dir)
     else:
-        print('Start to organize data')
+        print('Start to work on all data')
         dataset = {'images': [], 'annotations': [], 'categories': []}
         for i, cls in enumerate(classes, 0):
             dataset['categories'].append({'id': i, 'name': cls})
 
     obj_count = 0
-    for idx, file in enumerate(mmengine.track_iter_progress(indices)):
-        # support both .jpg and .png
-        """TODO:
+    for idx, image in enumerate(mmengine.track_iter_progress(indices)):
+        """Comments:
         1、replace may cause bug, while img filename is images1.jpg.
             Try to use os.path.splitext.
         2、It is better to use a format list like ['.jpg', '.png', '.jpeg']
             to verify file postfixes.
         3、use .lower() while verify file postfixes.
         """
-        txt_file = file.replace('images',
-                                'txt').replace('.jpg',
-                                               '.txt').replace('.png', '.txt')
-        img = mmcv.imread(osp.join(image_dir, 'images/') + file)
-        height, width = img.shape[:2]
+        # support both .jpg, .png, and .jpeg
+        img_suffix = osp.splitext(image)[1].lower()
+        if img_suffix in ['.jpg', '.png', '.jpeg']:
+            img_name = osp.splitext(image)[0]
+        img_info_dict, H, W = get_image_info(yolo_image_dir, idx, image)
 
         if split:
-            if file in train_img:
+            if image in train_img:
                 dataset = train_dataset
-            elif file in val_img:
+            elif image in val_img:
                 dataset = val_dataset
-            elif file in test_img:
+            elif image in test_img:
                 dataset = test_dataset
 
-        dataset['images'].append({
-            'file_name': file,
-            'id': idx,
-            'width': width,
-            'height': height
-        })
+        dataset['images'].append(img_info_dict)
 
-        if not osp.exists(osp.join(yolo_label_dir, txt_file)):
+        label_path = osp.join(yolo_label_dir, img_name) + '.txt'
+        if not osp.exists(label_path):
             # if current image is not annotated
-            # TODO: Add warning msg
+            print(f'WARNING: {label_path} does not exist. Skipped.')
             continue
 
-        # TODO: consider to excapsulate this part
-        with open(osp.join(yolo_label_dir, txt_file)) as f:
+        with open(label_path) as f:
             labels = f.readlines()
             for label in labels:
-                label = label.strip().split()
-                x = float(label[1])
-                y = float(label[2])
-                w = float(label[3])
-                h = float(label[4])
-
-                # convert x,y,w,h to x1,y1,x2,y2
-                H, W = img.shape[:2]
-                x1 = (x - w / 2) * W
-                y1 = (y - h / 2) * H
-                x2 = (x + w / 2) * W
-                y2 = (y + h / 2) * H
-
-                cls_id = int(label[0])
-                width = max(0., x2 - x1)
-                height = max(0., y2 - y1)
-                dataset['annotations'].append({
-                    'image_id':
-                    idx,
-                    'id':
-                    obj_count,
-                    'category_id':
-                    cls_id,
-                    'bbox': [x1, y1, width, height],
-                    'area':
-                    width * height,
-                    'segmentation': [[x1, y1, x2, y1, x2, y2, x1, y2]],
-                    'iscrowd':
-                    0
-                })
+                coco_info, obj_count = convert_bbox_info(
+                    label, idx, obj_count, H, W)
+                dataset['annotations'].append(coco_info)
                 obj_count += 1
 
-    # saving results
+    # saving results to result json in res_folder
     res_folder = osp.join(image_dir, 'coco_format')
     if not osp.exists(res_folder):
         os.makedirs(res_folder)
@@ -153,7 +175,7 @@ def convert_yolo_to_coco(image_dir: str, split: bool = False):
         out_file = osp.join(image_dir, 'coco_format/result.json')
         print(f'Saving converted annotations to {out_file}')
         mmengine.dump(dataset, out_file)
-    print('Finished')
+    print(f'Conversion is finished! Please check at {res_folder}!')
 
 
 if __name__ == '__main__':
