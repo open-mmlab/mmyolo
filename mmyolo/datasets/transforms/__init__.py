561aae593d8810c8bea2b83c59191913a7dc81e3
wanghonglie <wanghonglie@pjlab.org.cn>
2022-10-17 20:00:15 +0800

    refactor iou loss % rm v6affine

--- mmyolo/datasets/transforms/__init__.py
+++ mmyolo/datasets/transforms/__init__.py
@@ -1,10 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .mix_img_transforms import Mosaic, YOLOv5MixUp, YOLOXMixUp
 from .transforms import (LetterResize, LoadAnnotations, YOLOv5HSVRandomAug,
-                         YOLOv5KeepRatioResize, YOLOv5RandomAffine, YOLOv6RandomAffine)
+                         YOLOv5KeepRatioResize, YOLOv5RandomAffine)
 
 __all__ = [
     'YOLOv5KeepRatioResize', 'LetterResize', 'Mosaic', 'YOLOXMixUp',
     'YOLOv5MixUp', 'YOLOv5HSVRandomAug', 'LoadAnnotations',
-    'YOLOv5RandomAffine','YOLOv6RandomAffine'
+    'YOLOv5RandomAffine'
 ]
