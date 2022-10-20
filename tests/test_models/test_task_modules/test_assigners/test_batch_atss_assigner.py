979c32868cc3e10a43dd9ac225d4633c22ebd1ff
HinGwenWoong <peterhuang0323@qq.com>
2022-10-20 19:24:59 +0800

    Add UT

--- tests/test_models/test_task_modules/test_assigners/test_batch_atss_assigner.py
+++ tests/test_models/test_task_modules/test_assigners/test_batch_atss_assigner.py
@@ -2,7 +2,6 @@
 from unittest import TestCase
 
 import torch
-from mmengine.structures import InstanceData
 
 from mmyolo.models.task_modules.assigners import BatchATSSAssigner
 
@@ -10,138 +9,112 @@ from mmyolo.models.task_modules.assigners import BatchATSSAssigner
 class TestBatchATSSAssigner(TestCase):
 
     def test_batch_atss_assigner(self):
-        atss_assigner = BatchATSSAssigner(topk=9)
+        batch_atss_assigner = BatchATSSAssigner(topk=9,
+                                                iou2d_calculator=dict(type='mmdet.BboxOverlaps2D'),
+                                                num_classes=80)
+        batch_size = 16
         priors = torch.FloatTensor([
-            [0, 0, 10, 10],
-            [10, 10, 20, 20],
-            [5, 5, 15, 15],
-            [32, 32, 38, 42],
-        ])
+            [-16., -16., 24., 24.],
+            [-8., -16., 32., 24.],
+            [0., -16., 40., 24.],
+            [8., -16., 48., 24.]
+        ]).repeat(2100, 1)
         gt_bboxes = torch.FloatTensor([
-            [0, 0, 10, 9],
-            [0, 10, 10, 19],
-        ])
-        gt_labels = torch.LongTensor([2, 3])
+            [0, 0, 60, 93],
+            [229, 0, 532, 157],
+        ]).unsqueeze(0).repeat(batch_size, 1, 1)
+        gt_labels = torch.LongTensor([[0], [11]]).unsqueeze(0).repeat(batch_size, 1, 1)
+        num_level_bboxes = [6400, 1600, 400]
+        pad_bbox_flag = torch.FloatTensor([[1], [0]]).unsqueeze(0).repeat(batch_size, 1, 1)
+        pred_bboxes = torch.FloatTensor([
+            [-4., -4., 12., 12.],
+            [4., -4., 20., 12.],
+            [12., -4., 28., 12.],
+            [20., -4., 36., 12.]
+        ]).unsqueeze(0).repeat(batch_size, 2100, 1)
+        batch_assign_result = batch_atss_assigner.forward(priors, num_level_bboxes, gt_labels, gt_bboxes,
+                                                          pad_bbox_flag, pred_bboxes)
+        assigned_labels, assigned_bboxes, assigned_scores, fg_mask_pre_prior_bool = batch_assign_result
+        self.assertEqual(assigned_labels.shape, torch.Size([batch_size, 8400]))
+        self.assertEqual(assigned_bboxes.shape, torch.Size([batch_size, 8400, 4]))
+        self.assertEqual(assigned_scores.shape, torch.Size([batch_size, 8400, 80]))
+        self.assertEqual(fg_mask_pre_prior_bool.shape, torch.Size([batch_size, 8400]))
 
-        pred_instances = InstanceData(priors=priors)
-        gt_instances = InstanceData(bboxes=gt_bboxes, labels=gt_labels)
-        num_level_bboxes = [4]
-
-        assign_result = atss_assigner.assign(pred_instances, num_level_bboxes,
-                                             gt_instances)
-        self.assertEqual(len(assign_result.gt_inds), 4)
-        self.assertEqual(len(assign_result.labels), 4)
-
-        expected_gt_inds = torch.LongTensor([1, 0, 0, 0])
-        self.assertTrue(torch.all(assign_result.gt_inds == expected_gt_inds))
-
-    def test_atss_assigner_with_ignore(self):
-        atss_assigner = ATSSAssigner(topk=9)
-        priors = torch.FloatTensor([
-            [0, 0, 10, 10],
-            [10, 10, 20, 20],
-            [5, 5, 15, 15],
-            [30, 32, 40, 42],
-        ])
-        gt_bboxes = torch.FloatTensor([
-            [0, 0, 10, 9],
-            [0, 10, 10, 19],
-        ])
-        gt_labels = torch.LongTensor([2, 3])
-        gt_bboxes_ignore = torch.Tensor([
-            [30, 30, 40, 40],
-        ])
-
-        pred_instances = InstanceData(priors=priors)
-        gt_instances = InstanceData(bboxes=gt_bboxes, labels=gt_labels)
-        gt_instances_ignore = InstanceData(bboxes=gt_bboxes_ignore)
-        num_level_bboxes = [4]
-        assign_result = atss_assigner.assign(
-            pred_instances,
-            num_level_bboxes,
-            gt_instances,
-            gt_instances_ignore=gt_instances_ignore)
-
-        expected_gt_inds = torch.LongTensor([1, 0, 0, 0])
-        self.assertTrue(torch.all(assign_result.gt_inds == expected_gt_inds))
-
-    def test_atss_assigner_with_empty_gt(self):
+    def test_batch_atss_assigner_with_empty_gt(self):
         """Test corner case where an image might have no true detections."""
-        atss_assigner = ATSSAssigner(topk=9)
+        batch_atss_assigner = BatchATSSAssigner(topk=9,
+                                                iou2d_calculator=dict(type='mmdet.BboxOverlaps2D'),
+                                                num_classes=80)
+        batch_size = 16
         priors = torch.FloatTensor([
-            [0, 0, 10, 10],
-            [10, 10, 20, 20],
-            [5, 5, 15, 15],
-            [32, 32, 38, 42],
-        ])
-        gt_bboxes = torch.empty(0, 4)
-        gt_labels = torch.empty(0)
+            [-16., -16., 24., 24.],
+            [-8., -16., 32., 24.],
+            [0., -16., 40., 24.],
+            [8., -16., 48., 24.]
+        ]).repeat(2100, 1)
+        num_level_bboxes = [6400, 1600, 400]
+        pad_bbox_flag = torch.FloatTensor([[1], [0]]).unsqueeze(0).repeat(batch_size, 1, 1)
+        pred_bboxes = torch.FloatTensor([
+            [-4., -4., 12., 12.],
+            [4., -4., 20., 12.],
+            [12., -4., 28., 12.],
+            [20., -4., 36., 12.]
+        ]).unsqueeze(0).repeat(batch_size, 2100, 1)
 
-        pred_instances = InstanceData(priors=priors)
-        gt_instances = InstanceData(bboxes=gt_bboxes, labels=gt_labels)
-        num_level_bboxes = [4]
-        assign_result = atss_assigner.assign(pred_instances, num_level_bboxes,
-                                             gt_instances)
+        gt_bboxes = torch.empty(batch_size, 2, 4)
+        gt_labels = torch.empty(batch_size, 2, 1)
 
-        expected_gt_inds = torch.LongTensor([0, 0, 0, 0])
-        self.assertTrue(torch.all(assign_result.gt_inds == expected_gt_inds))
+        batch_assign_result = batch_atss_assigner.forward(priors, num_level_bboxes, gt_labels, gt_bboxes,
+                                                          pad_bbox_flag, pred_bboxes)
 
-    def test_atss_assigner_with_empty_boxes(self):
+        assigned_labels, assigned_bboxes, assigned_scores, fg_mask_pre_prior_bool = batch_assign_result
+        self.assertEqual(assigned_labels.shape, torch.Size([batch_size, 8400]))
+        self.assertEqual(assigned_bboxes.shape, torch.Size([batch_size, 8400, 4]))
+        self.assertEqual(assigned_scores.shape, torch.Size([batch_size, 8400, 80]))
+        self.assertEqual(fg_mask_pre_prior_bool.shape, torch.Size([batch_size, 8400]))
+
+        self.assertTrue(bool(batch_assign_result[1].all()) == 0.)
+        self.assertTrue(bool(batch_assign_result[2].all()) == 0.)
+        self.assertTrue(bool(batch_assign_result[3].all()) is False)
+
+    def test_batch_atss_assigner_with_empty_boxes(self):
         """Test corner case where a network might predict no boxes."""
-        atss_assigner = ATSSAssigner(topk=9)
-        priors = torch.empty((0, 4))
+        batch_atss_assigner = BatchATSSAssigner(topk=9,
+                                                iou2d_calculator=dict(type='mmdet.BboxOverlaps2D'),
+                                                num_classes=80)
+        batch_size = 16
+        priors = torch.empty(0, 1)
         gt_bboxes = torch.FloatTensor([
-            [0, 0, 10, 9],
-            [0, 10, 10, 19],
-        ])
-        gt_labels = torch.LongTensor([2, 3])
+            [0, 0, 60, 93],
+            [229, 0, 532, 157],
+        ]).unsqueeze(0).repeat(batch_size, 1, 1)
+        gt_labels = torch.LongTensor([[0], [11]]).unsqueeze(0).repeat(batch_size, 1, 1)
+        num_level_bboxes = [0, 0, 0]
+        pad_bbox_flag = torch.FloatTensor([[1], [0]]).unsqueeze(0).repeat(batch_size, 1, 1)
+        pred_bboxes = torch.FloatTensor([
+            [-4., -4., 12., 12.],
+            [4., -4., 20., 12.],
+            [12., -4., 28., 12.],
+            [20., -4., 36., 12.]
+        ]).unsqueeze(0).repeat(batch_size, 2100, 1)
 
-        pred_instances = InstanceData(priors=priors)
-        gt_instances = InstanceData(bboxes=gt_bboxes, labels=gt_labels)
-        num_level_bboxes = [0]
-        assign_result = atss_assigner.assign(pred_instances, num_level_bboxes,
-                                             gt_instances)
-        self.assertEqual(len(assign_result.gt_inds), 0)
-        self.assertTrue(tuple(assign_result.labels.shape) == (0, ))
+        with self.assertRaises(AssertionError):
+            batch_assign_result = batch_atss_assigner.forward(priors, num_level_bboxes, gt_labels, gt_bboxes,
+                                                              pad_bbox_flag, pred_bboxes)
 
-    def test_atss_assigner_with_empty_boxes_and_ignore(self):
-        """Test corner case where a network might predict no boxes and
-        ignore_iof_thr is on."""
-        atss_assigner = ATSSAssigner(topk=9)
-        priors = torch.empty((0, 4))
-        gt_bboxes = torch.FloatTensor([
-            [0, 0, 10, 9],
-            [0, 10, 10, 19],
-        ])
-        gt_bboxes_ignore = torch.Tensor([
-            [30, 30, 40, 40],
-        ])
-        gt_labels = torch.LongTensor([2, 3])
-
-        pred_instances = InstanceData(priors=priors)
-        gt_instances = InstanceData(bboxes=gt_bboxes, labels=gt_labels)
-        gt_instances_ignore = InstanceData(bboxes=gt_bboxes_ignore)
-        num_level_bboxes = [0]
-
-        assign_result = atss_assigner.assign(
-            pred_instances,
-            num_level_bboxes,
-            gt_instances,
-            gt_instances_ignore=gt_instances_ignore)
-        self.assertEqual(len(assign_result.gt_inds), 0)
-        self.assertTrue(tuple(assign_result.labels.shape) == (0, ))
-
-    def test_atss_assigner_with_empty_boxes_and_gt(self):
+    def test_batch_atss_assigner_with_empty_boxes_and_gt(self):
         """Test corner case where a network might predict no boxes and no
         gt."""
-        atss_assigner = ATSSAssigner(topk=9)
-        priors = torch.empty((0, 4))
-        gt_bboxes = torch.empty((0, 4))
-        gt_labels = torch.empty(0)
-        num_level_bboxes = [0]
-
-        pred_instances = InstanceData(priors=priors)
-        gt_instances = InstanceData(bboxes=gt_bboxes, labels=gt_labels)
-        assign_result = atss_assigner.assign(pred_instances, num_level_bboxes,
-                                             gt_instances)
-        self.assertEqual(len(assign_result.gt_inds), 0)
+        batch_atss_assigner = BatchATSSAssigner(topk=9,
+                                                iou2d_calculator=dict(type='mmdet.BboxOverlaps2D'),
+                                                num_classes=80)
+        batch_size = 16
+        priors = torch.empty(0, 1)
+        gt_bboxes = torch.empty(batch_size, 2, 4)
+        gt_labels = torch.empty(batch_size, 2, 1)
+        num_level_bboxes = [0, 0, 0]
+        pad_bbox_flag = torch.empty(batch_size, 2, 1)
+        pred_bboxes = torch.empty(batch_size, 8400, 4)
+        with self.assertRaises(AssertionError):
+            batch_assign_result = batch_atss_assigner.forward(priors, num_level_bboxes, gt_labels, gt_bboxes,
+                                                              pad_bbox_flag, pred_bboxes)
