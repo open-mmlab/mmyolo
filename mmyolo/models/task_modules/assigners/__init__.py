e5da7591f95eaba423f867b4d9104fb1b36ffdb2
HinGwenWoong <peterhuang0323@qq.com>
2022-10-21 16:04:16 +0800

    Fix lint

--- mmyolo/models/task_modules/assigners/__init__.py
+++ mmyolo/models/task_modules/assigners/__init__.py
@@ -3,5 +3,7 @@ from .batch_atss_assigner import BatchATSSAssigner
 from .batch_task_aligned_assigner import BatchTaskAlignedAssigner
 from .utils import select_candidates_in_gts, select_highest_overlaps
 
-__all__ = ['BatchATSSAssigner', 'BatchTaskAlignedAssigner',
-           'select_candidates_in_gts', 'select_highest_overlaps']
+__all__ = [
+    'BatchATSSAssigner', 'BatchTaskAlignedAssigner',
+    'select_candidates_in_gts', 'select_highest_overlaps'
+]
