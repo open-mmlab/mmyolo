c511d42fbde4a2e25e62306fab76a4adae2a3f8a
wanghonglie <wanghonglie@pjlab.org.cn>
2022-10-28 16:39:00 +0800

    refactor varifocal

--- mmyolo/models/losses/__init__.py
+++ mmyolo/models/losses/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .iou_loss import IoULoss, bbox_overlaps
-from .varifocal_loss import varifocal_loss
 
-__all__ = ['IoULoss', 'bbox_overlaps', 'varifocal_loss']
+__all__ = ['IoULoss', 'bbox_overlaps']
