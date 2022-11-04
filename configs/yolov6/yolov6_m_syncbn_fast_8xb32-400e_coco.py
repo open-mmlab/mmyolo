6e3a75f9508853f47bf4460c34117d90b556bb61
wanghonglie <wanghonglie@pjlab.org.cn>
2022-11-04 16:29:32 +0800

    align large test

--- configs/yolov6/yolov6_m_syncbn_fast_8xb32-400e_coco.py
+++ configs/yolov6/yolov6_m_syncbn_fast_8xb32-400e_coco.py
@@ -1,18 +1,20 @@
 _base_ = './yolov6_s_syncbn_fast_8xb32-400e_coco.py'
 
-deepen_factor = 1
-widen_factor = 1
+deepen_factor = 0.6
+widen_factor = 0.75
 
 model = dict(
     backbone=dict(
         type='YOLOv6CSPBep',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
+        expansion=float(2) / 3,
         block_cfg=dict(type='ConvWrapper')),
     neck=dict(
         type='YOLOv6CSPRepPAFPN',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
+        expansion=float(2) / 3,
         block_cfg=dict(type='ConvWrapper')),
     bbox_head=dict(
         type='YOLOv6Head',
