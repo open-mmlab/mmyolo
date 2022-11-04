10db73fdc8b76b008768456767d5b7cbf1bb5b49
wanghonglie <wanghonglie@pjlab.org.cn>
2022-11-04 14:09:36 +0800

    align larger infer

--- mmyolo/models/layers/__init__.py
+++ mmyolo/models/layers/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .ema import ExpMomentumEMA
-from .yolo_bricks import (EffectiveSELayer, ELANBlock,
+from .yolo_bricks import (BepC3StageBlock, EffectiveSELayer, ELANBlock,
                           MaxPoolAndStrideConvBlock, PPYOLOEBasicBlock,
                           RepStageBlock, RepVGGBlock, SPPFBottleneck,
                           SPPFCSPBlock)
@@ -8,5 +8,5 @@ from .yolo_bricks import (EffectiveSELayer, ELANBlock,
 __all__ = [
     'SPPFBottleneck', 'RepVGGBlock', 'RepStageBlock', 'ExpMomentumEMA',
     'ELANBlock', 'MaxPoolAndStrideConvBlock', 'SPPFCSPBlock',
-    'PPYOLOEBasicBlock', 'EffectiveSELayer'
+    'PPYOLOEBasicBlock', 'EffectiveSELayer', 'BepC3StageBlock'
 ]
