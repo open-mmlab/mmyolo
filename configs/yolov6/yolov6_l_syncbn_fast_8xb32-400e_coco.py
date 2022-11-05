9d90af1ce0e677325defad071f0157b726ad9cfe
wanghonglie <wanghonglie@pjlab.org.cn>
2022-11-05 21:29:48 +0800

    update l config

--- configs/yolov6/yolov6_l_syncbn_fast_8xb32-400e_coco.py
+++ configs/yolov6/yolov6_l_syncbn_fast_8xb32-400e_coco.py
@@ -1,4 +1,4 @@
-_base_ = './yolov6_s_syncbn_fast_8xb32-400e_coco.py'
+_base_ = './yolov6_m_syncbn_fast_8xb32-400e_coco.py'
 
 deepen_factor = 1
 widen_factor = 1
@@ -8,6 +8,7 @@ model = dict(
         type='YOLOv6CSPBep',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
+        expansion=1. / 2,
         block_cfg=dict(
             type='ConvWrapper',
             norm_cfg=dict(type='BN', momentum=0.03, eps=0.001)),
@@ -16,6 +17,7 @@ model = dict(
         type='YOLOv6CSPRepPAFPN',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
+        expansion=1. / 2,
         block_cfg=dict(
             type='ConvWrapper',
             norm_cfg=dict(type='BN', momentum=0.03, eps=0.001)),
