8e9eba43af610d531a9d9f119a7c2d9568e4119a
kitecats <90194592+kitecats@users.noreply.github.com>
2022-11-05 18:34:48 +0800

    Update attention_layers.py

--- mmyolo/models/layers/attention_layers.py
+++ mmyolo/models/layers/attention_layers.py
@@ -1,63 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
 from mmyolo.registry import MODELS
 import torch
 import torch.nn as nn
 from mmcv.cnn import ConvModule
 from mmengine.model import BaseModule
-from mmengine.utils import is_tuple_of
-
-
-@MODELS.register_module()
-class SELayer(BaseModule):
-    """Squeeze-and-Excitation Module.
-    Args:
-        in_channels (int): The input (and output) channels of the SE layer.
-        ratio (int): Squeeze ratio in SELayer, the intermediate channel will be
-            ``int(channels/ratio)``. Default: 16.
-        conv_cfg (None or dict): Config dict for convolution layer.
-            Default: None, which means using conv2d.
-        act_cfg (dict or Sequence[dict]): Config dict for activation layer.
-            If act_cfg is a dict, two activation layers will be configurated
-            by this dict. If act_cfg is a sequence of dicts, the first
-            activation layer will be configurated by the first dict and the
-            second activation layer will be configurated by the second dict.
-            Default: (dict(type='ReLU'), dict(type='Sigmoid'))
-        init_cfg (dict or list[dict], optional): Initialization config dict.
-            Default: None
-    """
-
-    def __init__(self,
-                 in_channels,
-                 ratio=16,
-                 conv_cfg=None,
-                 act_cfg=(dict(type='ReLU'), dict(type='Sigmoid')),
-                 init_cfg=None):
-        super(SELayer, self).__init__(init_cfg)
-        if isinstance(act_cfg, dict):
-            act_cfg = (act_cfg, act_cfg)
-        assert len(act_cfg) == 2
-        assert is_tuple_of(act_cfg, dict)
-        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
-        self.conv1 = ConvModule(
-            in_channels=in_channels,
-            out_channels=int(in_channels / ratio),
-            kernel_size=1,
-            stride=1,
-            conv_cfg=conv_cfg,
-            act_cfg=act_cfg[0])
-        self.conv2 = ConvModule(
-            in_channels=int(in_channels / ratio),
-            out_channels=in_channels,
-            kernel_size=1,
-            stride=1,
-            conv_cfg=conv_cfg,
-            act_cfg=act_cfg[1])
-
-    def forward(self, x):
-        out = self.global_avgpool(x)
-        out = self.conv1(out)
-        out = self.conv2(out)
-        return x * out
-
 
 class ChannelAttention(nn.Module):
     def __init__(self, channels, ratio=16):
