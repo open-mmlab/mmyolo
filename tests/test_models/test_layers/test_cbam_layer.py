76e4eb535c7ab3eb7b351d29183d11a92c6d293b
kitecats <2410778368@qq.com>
2022-11-07 22:18:49 +0800

    refine unit test

--- tests/test_models/test_layers/test_cbam_layer.py
+++ tests/test_models/test_layers/test_cbam_layer.py
@@ -1,3 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+
 from unittest import TestCase
 
 import torch
@@ -13,7 +15,7 @@ class TestCBAMLayer(TestCase):
     def test_forward(self):
         # kernel size must be 3 or 7
         with self.assertRaisesRegex(AssertionError,
-                                    "kernel size must be 3 or 7"):
+                                    'kernel size must be 3 or 7'):
             CBAMLayer(16, kernel_size=5)
 
         images = torch.randn(2, 16, 20, 20)
