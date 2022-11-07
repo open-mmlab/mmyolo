3407500bf6b1a8254c30185e2e4af057fecf5dd2
kitecats <2410778368@qq.com>
2022-11-07 21:09:53 +0800

    add docstring typehint

--- mmyolo/models/layers/cbam_layer.py
+++ mmyolo/models/layers/cbam_layer.py
@@ -7,7 +7,9 @@ from mmengine.model import BaseModule
 
 
 class ChannelAttention(nn.Module):
-    def __init__(self, channels, ratio=16):
+    def __init__(self,
+                 channels: int,
+                 ratio: int = 16):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
@@ -27,7 +29,7 @@ class ChannelAttention(nn.Module):
             act_cfg=None)
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         avgpool_out = self.conv2(self.conv1(self.avg_pool(x)))
         maxpool_out = self.conv2(self.conv1(self.max_pool(x)))
         out = self.sigmoid(avgpool_out + maxpool_out)
@@ -35,7 +37,7 @@ class ChannelAttention(nn.Module):
 
 
 class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=7):
+    def __init__(self, kernel_size: int = 7):
         super(SpatialAttention, self).__init__()
         assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
         self.conv = ConvModule(
@@ -47,7 +49,7 @@ class SpatialAttention(nn.Module):
             conv_cfg=None,
             act_cfg=dict(type='Sigmoid'))
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         avg_out = torch.mean(x, dim=1, keepdim=True)
         max_out, _ = torch.max(x, dim=1, keepdim=True)
         out = torch.cat([avg_out, max_out], dim=1)
@@ -68,16 +70,16 @@ class CBAMLayer(BaseModule):
     """
 
     def __init__(self,
-                 in_channels,
-                 ratio=16,
-                 kernel_size=7
+                 in_channels: int,
+                 ratio: int = 16,
+                 kernel_size: int = 7
                  ):
         super(CBAMLayer, self).__init__()
         self.channel_attention = ChannelAttention(channels=in_channels,
                                                   ratio=ratio)
         self.spatial_attention = SpatialAttention(kernel_size)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.channel_attention(x) * x
         out = self.spatial_attention(out) * out
         return out
