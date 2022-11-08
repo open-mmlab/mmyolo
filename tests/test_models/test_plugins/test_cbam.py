fdb6bfd3fe860c5969a3491abbbd1a1d68995616
kitecats <2410778368@qq.com>
2022-11-08 23:17:06 +0800

    refine cbam.py and plugins.md

--- tests/test_models/test_plugins/test_cbam.py
+++ tests/test_models/test_plugins/test_cbam.py
@@ -13,6 +13,21 @@ register_all_modules()
 class TestCBAM(TestCase):
 
     def test_forward(self):
+        with self.assertRaisesRegex(AssertionError):
+            # act_cfg in ChannelAttention sequence length must equal to 2
+            CBAM(
+                in_channels=16,
+                act_cfg=dict(
+                    ChannelAttention=(dict(type='ReLU'), ),
+                    SpatialAttention=dict(type='Sigmoid')))
+        with self.assertRaisesRegex(AssertionError):
+            # act_cfg in ChannelAttention sequence must be a tuple of dict
+            CBAM(
+                in_channels=16,
+                act_cfg=dict(
+                    ChannelAttention=[dict(type='ReLU'),
+                                      dict(type='Sigmoid')],
+                    SpatialAttention=dict(type='Sigmoid')))
 
         images = torch.randn(2, 16, 20, 20)
         cbam = CBAM(16)
@@ -21,5 +36,25 @@ class TestCBAM(TestCase):
 
         # test other ratio
         cbam = CBAM(16, ratio=8)
+        out = cbam(images)
+        self.assertEqual(out.shape, (2, 16, 20, 20))
+
+        # test other act_cfg in ChannelAttention
+        cbam = CBAM(
+            16,
+            ratio=8,
+            act_cfg=dict(
+                ChannelAttention=(dict(type='ReLU'), dict(type='HSigmoid')),
+                SpatialAttention=dict(type='Sigmoid')))
+        out = cbam(images)
+        self.assertEqual(out.shape, (2, 16, 20, 20))
+
+        # test other act_cfg in SpatialAttention
+        cbam = CBAM(
+            16,
+            ratio=8,
+            act_cfg=dict(
+                ChannelAttention=(dict(type='ReLU'), dict(type='Sigmoid')),
+                SpatialAttention=dict(type='HSigmoid')))
         out = cbam(images)
         self.assertEqual(out.shape, (2, 16, 20, 20))
