7ece6ea5e39ea97557a65ba7958177470737e7f6
kitecats <2410778368@qq.com>
2022-11-08 23:32:14 +0800

    fix error in test_cbam.py

--- tests/test_models/test_plugins/test_cbam.py
+++ tests/test_models/test_plugins/test_cbam.py
@@ -13,14 +13,20 @@ register_all_modules()
 class TestCBAM(TestCase):
 
     def test_forward(self):
-        with self.assertRaisesRegex(AssertionError):
+        with self.assertRaisesRegex(
+                AssertionError,
+                'act_cfg in ChannelAttention sequence length must equal to 2'):
             # act_cfg in ChannelAttention sequence length must equal to 2
             CBAM(
                 in_channels=16,
                 act_cfg=dict(
                     ChannelAttention=(dict(type='ReLU'), ),
                     SpatialAttention=dict(type='Sigmoid')))
-        with self.assertRaisesRegex(AssertionError):
+
+        with self.assertRaisesRegex(
+                AssertionError,
+                'act_cfg in ChannelAttention sequence must be a tuple of dict'
+        ):
             # act_cfg in ChannelAttention sequence must be a tuple of dict
             CBAM(
                 in_channels=16,
