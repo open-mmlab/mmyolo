b9d911476db3644d930cc1f7b65f5708f5f0c08a
wanghonglie <wanghonglie@pjlab.org.cn>
2022-11-11 17:18:18 +0800

    rm duplicate docde

--- mmyolo/models/necks/yolov6_pafpn.py
+++ mmyolo/models/necks/yolov6_pafpn.py
@@ -166,12 +166,15 @@ class YOLOv6RepPAFPN(BaseYOLONeck):
         return nn.Identity()
 
     def init_weights(self):
-        """Initialize the parameters."""
-        for m in self.modules():
-            if isinstance(m, torch.nn.Conv2d):
-                # In order to be consistent with the source code,
-                # reset the Conv2d initialization parameters
-                m.reset_parameters()
+        if self.init_cfg is None:
+            """Initialize the parameters."""
+            for m in self.modules():
+                if isinstance(m, torch.nn.Conv2d):
+                    # In order to be consistent with the source code,
+                    # reset the Conv2d initialization parameters
+                    m.reset_parameters()
+        else:
+            super().init_weights()
 
 
 @MODELS.register_module()
@@ -227,43 +230,6 @@ class YOLOv6CSPRepPAFPN(YOLOv6RepPAFPN):
             block_cfg=block_cfg,
             init_cfg=init_cfg)
 
-    def build_reduce_layer(self, idx: int) -> nn.Module:
-        """build reduce layer.
-
-        Args:
-            idx (int): layer idx.
-        Returns:
-            nn.Module: The reduce layer.
-        """
-        if idx == 2:
-            layer = ConvModule(
-                in_channels=int(self.in_channels[idx] * self.widen_factor),
-                out_channels=int(self.out_channels[idx - 1] *
-                                 self.widen_factor),
-                kernel_size=1,
-                stride=1,
-                norm_cfg=self.norm_cfg,
-                act_cfg=self.act_cfg)
-        else:
-            layer = nn.Identity()
-
-        return layer
-
-    def build_upsample_layer(self, idx: int) -> nn.Module:
-        """build upsample layer.
-
-        Args:
-            idx (int): layer idx.
-        Returns:
-            nn.Module: The upsample layer.
-        """
-        return nn.ConvTranspose2d(
-            in_channels=int(self.out_channels[idx - 1] * self.widen_factor),
-            out_channels=int(self.out_channels[idx - 1] * self.widen_factor),
-            kernel_size=2,
-            stride=2,
-            bias=True)
-
     def build_top_down_layer(self, idx: int) -> nn.Module:
         """build top down layer.
 
@@ -299,23 +265,6 @@ class YOLOv6CSPRepPAFPN(YOLOv6RepPAFPN):
                 act_cfg=self.act_cfg)
             return nn.Sequential(layer0, layer1)
 
-    def build_downsample_layer(self, idx: int) -> nn.Module:
-        """build downsample layer.
-
-        Args:
-            idx (int): layer idx.
-        Returns:
-            nn.Module: The downsample layer.
-        """
-        return ConvModule(
-            in_channels=int(self.out_channels[idx] * self.widen_factor),
-            out_channels=int(self.out_channels[idx] * self.widen_factor),
-            kernel_size=3,
-            stride=2,
-            padding=3 // 2,
-            norm_cfg=self.norm_cfg,
-            act_cfg=self.act_cfg)
-
     def build_bottom_up_layer(self, idx: int) -> nn.Module:
         """build bottom up layer.
 
@@ -334,15 +283,3 @@ class YOLOv6CSPRepPAFPN(YOLOv6RepPAFPN):
             hidden_ratio=self.hidden_ratio,
             norm_cfg=self.norm_cfg,
             act_cfg=self.block_act_cfg)
-
-    def build_out_layer(self, *args, **kwargs) -> nn.Module:
-        """build out layer."""
-        return nn.Identity()
-
-    def init_weights(self):
-        """Initialize the parameters."""
-        for m in self.modules():
-            if isinstance(m, torch.nn.Conv2d):
-                # In order to be consistent with the source code,
-                # reset the Conv2d initialization parameters
-                m.reset_parameters()
