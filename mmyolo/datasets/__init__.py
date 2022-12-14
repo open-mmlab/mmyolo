03e31ef4474b1f1ef6e14bd8cff3574616e63f71
gulingrui <379319054@qq.com>
2022-12-14 21:39:34 +0800

    format code

--- mmyolo/datasets/__init__.py
+++ mmyolo/datasets/__init__.py
@@ -1,12 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .tmp_coco import TmpCocoDataset, PPYOLOEResize, PPYOLOENormalizeImage
 from .transforms import *  # noqa: F401,F403
-from .utils import BatchShapePolicy, ppyoloe_collate, yolov5_collate, ppyoloe_collate_temp
+from .utils import BatchShapePolicy, ppyoloe_collate, yolov5_collate
 from .yolov5_coco import YOLOv5CocoDataset
 from .yolov5_voc import YOLOv5VOCDataset
 
 __all__ = [
     'YOLOv5CocoDataset', 'YOLOv5VOCDataset', 'BatchShapePolicy',
-    'yolov5_collate', 'ppyoloe_collate', 'TmpCocoDataset',
-    'ppyoloe_collate_temp', 'PPYOLOEResize', 'PPYOLOENormalizeImage'
+    'yolov5_collate', 'ppyoloe_collate'
 ]
--- mmyolo/models/data_preprocessors/__init__.py
+++ mmyolo/models/data_preprocessors/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .data_preprocessor import (PPYOLOEBatchSyncRandomResize,
+                                PPYOLOEBatchSyncRandomResizeallopencv,
                                 PPYOLOEDetDataPreprocessor,
                                 YOLOv5DetDataPreprocessor)
 
 __all__ = [
     'YOLOv5DetDataPreprocessor', 'PPYOLOEDetDataPreprocessor',
-    'PPYOLOEBatchSyncRandomResize'
+    'PPYOLOEBatchSyncRandomResize', 'PPYOLOEBatchSyncRandomResizeallopencv'
 ]
