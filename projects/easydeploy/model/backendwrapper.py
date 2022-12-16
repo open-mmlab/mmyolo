3e61bfb867a34cf89305e192f09c5b212d23c8c2
triple-Mu <gpu@163.com>
2022-12-16 12:42:31 +0800

    Add ort wrapper

--- projects/easydeploy/model/backendwrapper.py
+++ projects/easydeploy/model/backendwrapper.py
@@ -1,7 +1,11 @@
 import warnings
+from collections import namedtuple
 from functools import partial
 from pathlib import Path
 from typing import List, Optional, Union
+
+import numpy as np
+import onnxruntime
 
 try:
     import tensorrt as trt
@@ -12,13 +16,19 @@ import torch
 warnings.filterwarnings(action='ignore', category=DeprecationWarning)
 
 
-class BackendWrapper(torch.nn.Module):
+class TRTWrapper(torch.nn.Module):
 
     def __init__(self, weight: Union[str, Path],
                  device: Optional[torch.device]):
         super().__init__()
-        self.weight = Path(weight) if isinstance(weight, str) else weight
-        self.device = device if device is not None else torch.device('cuda:0')
+        weight = Path(weight) if isinstance(weight, str) else weight
+        assert weight.exists() and weight.suffix in ('.engine', '.plan')
+        if isinstance(device, str):
+            device = torch.device(device)
+        elif isinstance(device, int):
+            device = torch.device(f'cuda:{device}')
+        self.weight = weight
+        self.device = device
         self.stream = torch.cuda.Stream(device=device)
         self.__init_engine()
         self.__init_bindings()
@@ -132,3 +142,68 @@ class BackendWrapper(torch.nn.Module):
             return torch.float32
         else:
             raise TypeError(f'{dtype} is not supported by torch')
+
+
+class ORTWrapper(torch.nn.Module):
+
+    def __init__(self, weight: Union[str, Path],
+                 device: Optional[torch.device]):
+        super().__init__()
+        weight = Path(weight) if isinstance(weight, str) else weight
+        assert weight.exists() and weight.suffix == '.onnx'
+
+        if isinstance(device, str):
+            device = torch.device(device)
+        elif isinstance(device, int):
+            device = torch.device(f'cuda:{device}')
+        self.weight = weight
+        self.device = device
+        self.__init_session()
+        self.__init_bindings()
+
+    def __init_session(self):
+        providers = ['CPUExecutionProvider']
+        if 'cuda' in self.device.type:
+            providers.insert(0, 'CUDAExecutionProvider')
+
+        session = onnxruntime.InferenceSession(
+            str(self.weight), providers=providers)
+        self.session = session
+
+    def __init_bindings(self):
+        Binding = namedtuple('Binding', ('name', 'dtype', 'shape'))
+        inputs_info = []
+        outputs_info = []
+        self.is_dynamic = False
+        for i, tensor in enumerate(self.session.get_inputs()):
+            if any(not isinstance(i, int) for i in tensor.shape):
+                self.is_dynamic = True
+            inputs_info.append(
+                Binding(tensor.name, tensor.dtype, tensor.shape))
+
+        for i, tensor in enumerate(self.session.get_outputs()):
+            outputs_info.append(
+                Binding(tensor.name, tensor.dtype, tensor.shape))
+        self.inputs_info = inputs_info
+        self.outputs_info = outputs_info
+        self.num_inputs = len(inputs_info)
+
+    def forward(self, *inputs):
+
+        assert len(inputs) == self.num_inputs
+
+        contiguous_inputs: List[np.ndarray] = [
+            i.contiguous().cpu().numpy() for i in inputs
+        ]
+
+        if not self.is_dynamic:
+            # make sure input shape is right for static input shape
+            for i in range(self.num_inputs):
+                assert contiguous_inputs[i].shape == self.inputs_info[i].shape
+
+        outputs = self.session.run([o.name for o in self.outputs_info], {
+            j.name: contiguous_inputs[i]
+            for i, j in enumerate(self.inputs_info)
+        })
+
+        return tuple(outputs)
