09cb5a8fa18d46ab7d13c7b7e0238a6b7da75685
Nioolek <379319054@qq.com>
2023-01-03 16:21:54 +0800

    add imagenet model convert and use init_cfg to init backbone

--- configs/ppyoloe/ppyoloe_l_fast_8xb20-300e_coco.py
+++ configs/ppyoloe/ppyoloe_l_fast_8xb20-300e_coco.py
@@ -1,4 +1,7 @@
 _base_ = './ppyoloe_s_fast_8xb32-300e_coco.py'
+# The pretrained model is geted and converted from official PPYOLOE.
+# https://github.com/PaddlePaddle/PaddleDetection/blob/release/2.5/configs/ppyoloe/README.md
+checkpoint = 'https://download.openmmlab.com/mmyolo/v0/ppyoloe/ppyoloe_pretrain/cspresnet_l_imagenet1k_pretrained-c0010e6c.pth'  # noqa
 
 deepen_factor = 1.0
 widen_factor = 1.0
@@ -14,5 +17,3 @@ model = dict(
     bbox_head=dict(head_module=dict(widen_factor=widen_factor)))
 
 train_dataloader = dict(batch_size=train_batch_size_per_gpu)
-
-load_from = 'https://download.openmmlab.com/mmyolo/v0/ppyoloe/ppyoloe_pretrain/cspresnet_l_imagenet1k_pretrained-c0010e6c.pth'  # noqa
