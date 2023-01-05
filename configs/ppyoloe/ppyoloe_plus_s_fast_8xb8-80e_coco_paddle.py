e146fbe099da23083e568d0c00ee2b4ccd2a6e56
Nioolek <379319054@qq.com>
2023-01-05 14:06:50 +0800

    add official version ppyoloe

---
+++ configs/ppyoloe/ppyoloe_plus_s_fast_8xb8-80e_coco_paddle.py
@@ -0,0 +1,23 @@
+_base_ = './ppyoloe_plus_s_fast_8xb8-80e_coco.py'
+
+# dataset settings
+dataset_type = 'PPYOLOECocoDataset'
+
+model = dict(
+    data_preprocessor=dict(
+        batch_augments=[
+            dict(
+                type='PPYOLOEBatchSyncRandomResizeallopencv',
+                random_size_range=(320, 800),
+                interval=1,
+                size_divisor=32,
+                random_interp=True,
+                keep_ratio=False)
+        ],
+        bgr_to_rgb=True))
+
+train_dataloader = dict(dataset=dict(type=dataset_type))
+
+val_dataloader = dict(dataset=dict(type=dataset_type))
+
+test_dataloader = val_dataloader
--- mmyolo/models/data_preprocessors/__init__.py
+++ mmyolo/models/data_preprocessors/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .data_preprocessor import (PPYOLOEBatchRandomResize,
+                                PPYOLOEBatchSyncRandomResizeallopencv,
                                 PPYOLOEDetDataPreprocessor,
                                 YOLOv5DetDataPreprocessor)
 
 __all__ = [
     'YOLOv5DetDataPreprocessor', 'PPYOLOEDetDataPreprocessor',
-    'PPYOLOEBatchRandomResize'
+    'PPYOLOEBatchRandomResize', 'PPYOLOEBatchSyncRandomResizeallopencv'
 ]
