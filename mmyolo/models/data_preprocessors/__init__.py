8127805dd3252acd09b86124e94a8778150e179e
Nioolek <40284075+Nioolek@users.noreply.github.com>
2023-01-06 15:54:39 +0800

    [Feature] Support PPYOLOE training (#259)
    * add ppyoloe backbone, neck
    
    * add ppyoloe test
    
    * add docstring
    
    * add ppyoloe m/l/x configfile
    
    * add ppyoloe_coco.py
    
    * rename config
    
    * add typehint
    
    * format code; add ut
    
    * add datapre
    
    * add datapre
    
    * add ppyoloe datapre
    
    * add ppyoloe datapre
    
    * add ppyoloe datapre
    
    * reproduce coco v0.1
    
    * add ut
    
    * add ut, docstring
    
    * fix transforms bug
    
    * use mmdet dfloss
    
    * add non plus model config
    
    * add non plus model config
    
    * fix
    
    * add ut
    
    * produce coco v0.2
    
    * fix config
    
    * fix config
    
    * fix eps and transforms bug
    
    * add ema
    
    * fix resize
    
    * fix transforms.py
    
    * fix transforms.py
    
    * fix transforms.py
    
    * old version
    
    * old version
    
    * old version
    
    * old version
    
    * old version
    
    * old version
    
    * fix stride loss error
    
    * add INTER_LANCZOS4
    
    * fix crop bug
    
    * init commit
    
    * format code
    
    * format code
    
    * bgr transforms.py
    
    * add typehint and doc in transforms.py
    
    * 继承新版yolov6head写法，删除不必要的注释
    
    * fix transforms var name bug
    
    * bbox decode use stridetensor insted of priors
    
    * add headmodule todo
    
    * add ppyoloe README.md
    
    * add ppyoloe README.md
    
    * Update tests/test_datasets/test_transforms/test_transforms.py
    
    Co-authored-by: Range King <RangeKingHZ@gmail.com>
    
    * Update tests/test_datasets/test_transforms/test_transforms.py
    
    Co-authored-by: Range King <RangeKingHZ@gmail.com>
    
    * save ckpt last 10 epochs
    
    * save_best ckpt
    
    * del ppyoloe collate
    
    * change name of ppyoloebatchrandomresize
    
    * add the reason for rewritten PPYOLOEDetDataPreprocessor
    
    * solve ppyoloerandomresize name error
    
    * rm PPYOLOERandomExpand
    
    * rm l1 loss
    
    * rm yolov6 loss_obj
    
    * add difference between yolov6 and ppyoloe
    
    * add reason for rewrite paramscheduler
    
    * change proj init way
    
    * fix error
    
    * rm proj_conv in pth
    
    * format code
    
    * add load_from
    
    * update
    
    * support fast training
    
    * add pretrained model url
    
    * update
    
    * add pretrained model url
    
    * fix error
    
    * add imagenet model convert and use init_cfg to init backbone
    
    * add plus model pretrain model
    
    * add ut
    
    * add ut
    
    * fix ut
    
    * fix withstride bug
    
    * cat in yolov5_collate
    
    * merge
    
    * fix typehint
    
    * update readme
    
    * add reason for gap
    
    * fix log in README.md
    
    * rollback yolov6
    
    * change inherit
    
    * fix ut
    
    * fix ut
    
    Co-authored-by: Range King <RangeKingHZ@gmail.com>
    Co-authored-by: hha <1286304229@qq.com>
    Co-authored-by: huanghaian <huanghaian@sensetime.com>

--- mmyolo/models/data_preprocessors/__init__.py
+++ mmyolo/models/data_preprocessors/__init__.py
@@ -1,4 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .data_preprocessor import YOLOv5DetDataPreprocessor
+from .data_preprocessor import (PPYOLOEBatchRandomResize,
+                                PPYOLOEDetDataPreprocessor,
+                                YOLOv5DetDataPreprocessor)
 
-__all__ = ['YOLOv5DetDataPreprocessor']
+__all__ = [
+    'YOLOv5DetDataPreprocessor', 'PPYOLOEDetDataPreprocessor',
+    'PPYOLOEBatchRandomResize'
+]
