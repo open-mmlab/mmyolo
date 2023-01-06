0f687715cd30bc97932c22838efadf1528f3b163
Nioolek <379319054@qq.com>
2023-01-06 14:43:57 +0800

    change inherit

--- mmyolo/datasets/transforms/transforms.py
+++ mmyolo/datasets/transforms/transforms.py
@@ -9,7 +9,6 @@ import torch
 from mmcv.transforms import BaseTransform
 from mmcv.transforms.utils import cache_randomness
 from mmdet.datasets.transforms import LoadAnnotations as MMDET_LoadAnnotations
-from mmdet.datasets.transforms import RandomCrop as MMDET_RandomCrop
 from mmdet.datasets.transforms import Resize as MMDET_Resize
 from mmdet.structures.bbox import (HorizontalBoxes, autocast_box_type,
                                    get_box_type)
@@ -802,7 +801,7 @@ class PPYOLOERandomDistort(BaseTransform):
 
 
 @TRANSFORMS.register_module()
-class PPYOLOERandomCrop(MMDET_RandomCrop):
+class PPYOLOERandomCrop(BaseTransform):
     """Random crop the img and bboxes. Different thresholds are used in PPYOLOE
     to judge whether the clipped image meets the requirements. This
     implementation is different from the implementation of RandomCrop in mmdet.
@@ -974,6 +973,23 @@ class PPYOLOERandomCrop(MMDET_RandomCrop):
                 results = self._crop_data(results, crop_box, valid_inds)
                 return results
         return results
+
+    @cache_randomness
+    def _rand_offset(self, margin: Tuple[int, int]) -> Tuple[int, int]:
+        """Randomly generate crop offset.
+
+        Args:
+            margin (Tuple[int, int]): The upper bound for the offset generated
+                randomly.
+
+        Returns:
+            Tuple[int, int]: The random offset for the crop.
+        """
+        margin_h, margin_w = margin
+        offset_h = np.random.randint(0, margin_h + 1)
+        offset_w = np.random.randint(0, margin_w + 1)
+
+        return (offset_h, offset_w)
 
     @cache_randomness
     def _get_crop_size(self, image_size: Tuple[int, int]) -> Tuple[int, int]:
