152cfe926604a5ad6035e581c85e8bcaa22916ef
HinGwenWoong <peterhuang0323@qq.com>
2023-01-10 14:37:46 +0800

    Add model converter

---
+++ tools/model_converters/yolov8_to_mmyolo.py
@@ -0,0 +1,91 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import argparse
+from collections import OrderedDict
+
+import torch
+
+convert_dict_s = {
+    # backbone
+    'model.0': 'backbone.stem',
+    'model.1': 'backbone.stage1.0',
+    'model.2': 'backbone.stage1.1',
+    'model.3': 'backbone.stage2.0',
+    'model.4': 'backbone.stage2.1',
+    'model.5': 'backbone.stage3.0',
+    'model.6': 'backbone.stage3.1',
+    'model.7': 'backbone.stage4.0',
+    'model.8': 'backbone.stage4.1',
+    'model.9': 'backbone.stage4.2',
+
+    # neck
+    'model.12': 'neck.top_down_layers.0',
+    'model.15': 'neck.top_down_layers.1',
+    'model.16': 'neck.downsample_layers.0',
+    'model.18': 'neck.bottom_up_layers.0',
+    'model.19': 'neck.downsample_layers.1',
+    'model.21': 'neck.bottom_up_layers.1',
+
+    # Detector
+    'model.22': 'bbox_head.head_module',
+}
+
+
+def convert(src, dst):
+    """Convert keys in pretrained YOLOv5 models to mmyolo style."""
+    convert_dict = convert_dict_s
+
+    # if src.endswith('6.pt'):
+    #     convert_dict = convert_dict_p6
+    #     is_p6_model = True
+    #     print('Converting P6 model')
+    # else:
+    #     convert_dict = convert_dict_p5
+    #     is_p6_model = False
+    #     print('Converting P5 model')
+    try:
+        yolov8_model = torch.load(src)['model']
+        blobs = yolov8_model.state_dict()
+    except ModuleNotFoundError:
+        raise RuntimeError(
+            'This script must be placed under the ultralytics/yolov5 repo,'
+            ' because loading the official pretrained model need'
+            ' `model.py` to build model.'
+            'Also need to install hydra-core>=1.2.0 and thop>=0.1.1')
+    state_dict = OrderedDict()
+
+    for key, weight in blobs.items():
+        num, module = key.split('.')[1:3]
+        prefix = f'model.{num}'
+        new_key = key.replace(prefix, convert_dict[prefix])
+
+        if '.m.' in new_key:
+            new_key = new_key.replace('.m.', '.blocks.')
+            new_key = new_key.replace('.cv', '.conv')
+        elif '.bbox_head.head_module.' in new_key:
+            new_key = new_key.replace('.cv2', '.cls_preds')
+            new_key = new_key.replace('.cv3', '.reg_preds')
+        else:
+            new_key = new_key.replace('.cv1', '.main_conv')
+            new_key = new_key.replace('.cv2', '.final_conv')
+
+        state_dict[new_key] = weight
+        print(f'Convert {key} to {new_key}')
+
+    # save checkpoint
+    checkpoint = dict()
+    checkpoint['state_dict'] = state_dict
+    torch.save(checkpoint, dst)
+
+
+# Note: This script must be placed under the yolov5 repo to run.
+def main():
+    parser = argparse.ArgumentParser(description='Convert model keys')
+    parser.add_argument(
+        '--src', default='work_dirs/offical_yolov8s.pt', help='src yolov8 model path')
+    parser.add_argument('--dst', default='work_dirs/mmyolov8s.pt', help='save path')
+    args = parser.parse_args()
+    convert(args.src, args.dst)
+
+
+if __name__ == '__main__':
+    main()
