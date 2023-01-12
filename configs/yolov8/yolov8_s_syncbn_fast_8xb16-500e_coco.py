114f75c5ea7fa4a7702b08369d16a395248819ed
huanghaian <huanghaian@sensetime.com>
2023-01-12 19:10:25 +0800

    update max_aspect_ratio

--- configs/yolov8/yolov8_s_syncbn_fast_8xb16-500e_coco.py
+++ configs/yolov8/yolov8_s_syncbn_fast_8xb16-500e_coco.py
@@ -122,6 +122,7 @@ last_transform = [
         max_rotate_degree=0.0,
         max_shear_degree=0.0,
         scaling_ratio_range=(0.5, 1.5),
+        max_aspect_ratio=100,
         # img_scale is (width, height)
         border=(-img_scale[0] // 2, -img_scale[1] // 2),
         border_val=(114, 114, 114)),
