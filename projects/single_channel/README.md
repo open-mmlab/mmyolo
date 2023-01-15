34ad2816c77031dde895587dbab84886858c9de4
landhill <577572046@qq.com>
2023-01-15 19:55:54 +0800

    [Docs]Add an example project to 'projects' folder (#409)

--- projects/single_channel/README.md
+++ projects/single_channel/README.md
@@ -1,4 +1,4 @@
-# MMYOLO Model #409
+# Training on a single channel image dataset
 
 ## Introduction
 
@@ -15,12 +15,18 @@ If you are using a custom grayscale image dataset, you can skip this step.
 ```shell
 python tools/misc/download_dataset.py --dataset-name balloon --save-dir projects/single_channel/data --unzip
 python projects/single_channel/balloon2coco_single_channel.py
-python projects/single_channel/single_channel.py
+```
+
+Run the following command to replace the dataset with a single channel dataset.
+
+```shell
+python projects/single_channel/single_channel.py --path projects/single_channel/data/balloon
+# --path  *Original dataset path
 ```
 
 ### Training
 
-In the configuration file, `_base_` and `data_root` need to be modified to the corresponding dataset paths.
+In the `yolov5_s-v61_syncbn_fast_1xb4-300e_balloon_single_channel.py`, `_base_` and `data_root` need to be modified to the corresponding dataset paths. There should be `train`, `train.json`, `val`, `val.json` under the dataset file.
 
 ```shell
 python tools/train.py projects/single_channel/configs/yolov5_s-v61_syncbn_fast_1xb4-300e_balloon_single_channel.py
@@ -29,7 +35,11 @@ python tools/train.py projects/single_channel/configs/yolov5_s-v61_syncbn_fast_1
 ### Model Testing
 
 ```shell
-python tools/test.py projects/single_channel/configs/yolov5/yolov5_s-v61_syncbn_fast_1xb4-300e_balloon.py \
-                     work_dirs/yolov5_s-v61_syncbn_fast_1xb4-300e_balloon/epoch_300.pth \
+python tools/test.py projects/single_channel/configs/yolov5_s-v61_syncbn_fast_1xb4-300e_balloon_single_channel.py \
+                     work_dirs/yolov5_s-v61_syncbn_fast_1xb4-300e_balloon_single_channel/epoch_300.pth \
                      --show-dir show_results
 ```
+
+<img src="https://raw.githubusercontent.com/landhill/mmyolo/main/resources/single_channel_test.jpg"/>
+
+The left picture shows the physical labeling, and the right picture shows the target detection results.
--- projects/single_channel/single_channel.py
+++ projects/single_channel/single_channel.py
@@ -1,29 +1,47 @@
 import imghdr
 import os
+import argparse
 from typing import List
 
 from PIL import Image
 
-path = 'projects/single_channel/data/balloon/train'
-save_path = 'projects/single_channel/data/balloon/train'
-file_list: List[str] = os.listdir(path)
 
-# Grayscale conversion of each image
-for file in file_list:
-    if imghdr.what(path + '/' + file) != 'jpeg':
-        continue
-    o_img = Image.open(path + '/' + file)
-    L_img = o_img.convert('L')
-    L_img.save(save_path + '/' + file)
+def parse_args():
+    parser = argparse.ArgumentParser(description='data_path')
+    parser.add_argument(
+        "--path",
+        type=str,
+        default="projects/single_channel/data/balloon",
+        help="Original dataset path")
+    return parser.parse_args()
 
-path = 'projects/single_channel/data/balloon/val'
-save_path = 'projects/single_channel/data/balloon/val'
-file_list = os.listdir(path)
 
-# Grayscale conversion of each image
-for file in file_list:
-    if imghdr.what(path + '/' + file) != 'jpeg':
-        continue
-    o_img = Image.open(path + '/' + file)
-    L_img = o_img.convert('L')
-    L_img.save(save_path + '/' + file)
+def main():
+    args = parse_args()
+
+    path = args.path + '/train/'
+    save_path = path
+    file_list: List[str] = os.listdir(path)
+    # Grayscale conversion of each imager
+    for file in file_list:
+        if imghdr.what(path + '/' + file) != 'jpeg':
+            continue
+        o_img = Image.open(path + '/' + file)
+        L_img = o_img.convert('L')
+        L_img.save(save_path + '/' + file)
+        args = parse_args()
+
+    path = args.path + '/val/'
+    save_path = path
+    file_list: List[str] = os.listdir(path)
+    # Grayscale conversion of each imager
+    for file in file_list:
+        if imghdr.what(path + '/' + file) != 'jpeg':
+            continue
+        o_img = Image.open(path + '/' + file)
+        L_img = o_img.convert('L')
+        L_img.save(save_path + '/' + file)
+
+
+if (__name__ == "__main__"):
+    main()
