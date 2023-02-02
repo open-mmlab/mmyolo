50ef68b5c1f2366e9c3705f7e732fc8ac382a664
huanghaian <huanghaian@sensetime.com>
2023-02-02 19:14:58 +0800

    refactor_config

--- configs/yolov5/yolov5_s-v61_syncbn_8xb16-300e_coco.py
+++ configs/yolov5/yolov5_s-v61_syncbn_8xb16-300e_coco.py
@@ -1,26 +1,44 @@
 _base_ = '../_base_/default_runtime.py'
 
-# dataset settings
+# -------------------Frequently modified parameters-----------------------
+# -----data related-----
 data_root = 'data/coco/'
-dataset_type = 'YOLOv5CocoDataset'
+train_ann_file = 'annotations/instances_train2017.json'
+train_data_prefix = 'train2017/'
+val_ann_file = 'annotations/instances_val2017.json'
+val_data_prefix = 'val2017/'
 
-# parameters that often need to be modified
 num_classes = 80
-img_scale = (640, 640)  # width, height
-deepen_factor = 0.33
-widen_factor = 0.5
-max_epochs = 300
-save_epoch_intervals = 10
 train_batch_size_per_gpu = 16
 train_num_workers = 8
-val_batch_size_per_gpu = 1
-val_num_workers = 2
-
 # persistent_workers must be False if num_workers is 0.
 persistent_workers = True
 
+# -----model related-----
+anchors = [
+    [(10, 13), (16, 30), (33, 23)],  # P3/8
+    [(30, 61), (62, 45), (59, 119)],  # P4/16
+    [(116, 90), (156, 198), (373, 326)]  # P5/32
+]
+
+# -----train val related-----
 # Base learning rate for optim_wrapper
 base_lr = 0.01
+max_epochs = 300
+
+model_test_cfg = dict(
+    multi_label=True,
+    nms_pre=30000,
+    score_thr=0.001,
+    nms=dict(type='nms', iou_threshold=0.65),
+    max_per_img=300)
+
+# -------------------Possible modified parameters-----------------------
+# -----data related-----
+img_scale = (640, 640)  # width, height
+dataset_type = 'YOLOv5CocoDataset'
+val_batch_size_per_gpu = 1
+val_num_workers = 2
 
 # only on Val
 batch_shapes_cfg = dict(
@@ -30,18 +48,30 @@ batch_shapes_cfg = dict(
     size_divisor=32,
     extra_pad_ratio=0.5)
 
-anchors = [
-    [(10, 13), (16, 30), (33, 23)],  # P3/8
-    [(30, 61), (62, 45), (59, 119)],  # P4/16
-    [(116, 90), (156, 198), (373, 326)]  # P5/32
-]
+# -----model related-----
+deepen_factor = 0.33
+widen_factor = 0.5
 strides = [8, 16, 32]
 num_det_layers = 3
+norm_cfg = dict(type='BN', momentum=0.03, eps=0.001)
 
+loss_cls_weight = 0.5
+loss_bbox_weight = 0.05
+loss_obj_weight = 1.0
+prior_match_thr = 4.
+obj_level_weights = [4., 1., 0.4]
+lr_factor = 0.01
+weight_decay = 0.0005
+max_keep_ckpts = 3
+affine_scale = 0.5
+
+# -----train val related-----
+save_epoch_intervals = 10
 # single-scale training is recommended to
 # be turned on, which can speed up training.
 env_cfg = dict(cudnn_benchmark=True)
 
+# -------------------------------------------------------------------
 model = dict(
     type='YOLODetector',
     data_preprocessor=dict(
@@ -53,7 +83,7 @@ model = dict(
         type='YOLOv5CSPDarknet',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
-        norm_cfg=dict(type='BN', momentum=0.03, eps=0.001),
+        norm_cfg=norm_cfg,
         act_cfg=dict(type='SiLU', inplace=True)),
     neck=dict(
         type='YOLOv5PAFPN',
@@ -62,7 +92,7 @@ model = dict(
         in_channels=[256, 512, 1024],
         out_channels=[256, 512, 1024],
         num_csp_blocks=3,
-        norm_cfg=dict(type='BN', momentum=0.03, eps=0.001),
+        norm_cfg=norm_cfg,
         act_cfg=dict(type='SiLU', inplace=True)),
     bbox_head=dict(
         type='YOLOv5Head',
@@ -82,28 +112,25 @@ model = dict(
             type='mmdet.CrossEntropyLoss',
             use_sigmoid=True,
             reduction='mean',
-            loss_weight=0.5 * (num_classes / 80 * 3 / num_det_layers)),
+            loss_weight=loss_cls_weight *
+            (num_classes / 80 * 3 / num_det_layers)),
         loss_bbox=dict(
             type='IoULoss',
             iou_mode='ciou',
             bbox_format='xywh',
             eps=1e-7,
             reduction='mean',
-            loss_weight=0.05 * (3 / num_det_layers),
+            loss_weight=loss_bbox_weight * (3 / num_det_layers),
             return_iou=True),
         loss_obj=dict(
             type='mmdet.CrossEntropyLoss',
             use_sigmoid=True,
             reduction='mean',
-            loss_weight=1.0 * ((img_scale[0] / 640)**2 * 3 / num_det_layers)),
-        prior_match_thr=4.,
-        obj_level_weights=[4., 1., 0.4]),
-    test_cfg=dict(
-        multi_label=True,
-        nms_pre=30000,
-        score_thr=0.001,
-        nms=dict(type='nms', iou_threshold=0.65),
-        max_per_img=300))
+            loss_weight=loss_obj_weight *
+            ((img_scale[0] / 640)**2 * 3 / num_det_layers)),
+        prior_match_thr=prior_match_thr,
+        obj_level_weights=obj_level_weights),
+    test_cfg=model_test_cfg)
 
 albu_train_transforms = [
     dict(type='Blur', p=0.01),
@@ -128,7 +155,7 @@ train_pipeline = [
         type='YOLOv5RandomAffine',
         max_rotate_degree=0.0,
         max_shear_degree=0.0,
-        scaling_ratio_range=(0.5, 1.5),
+        scaling_ratio_range=(1 - affine_scale, 1 + affine_scale),
         # img_scale is (width, height)
         border=(-img_scale[0] // 2, -img_scale[1] // 2),
         border_val=(114, 114, 114)),
@@ -160,8 +187,8 @@ train_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        ann_file='annotations/instances_train2017.json',
-        data_prefix=dict(img='train2017/'),
+        ann_file=train_ann_file,
+        data_prefix=dict(img=train_data_prefix),
         filter_cfg=dict(filter_empty_gt=False, min_size=32),
         pipeline=train_pipeline))
 
@@ -191,8 +218,8 @@ val_dataloader = dict(
         type=dataset_type,
         data_root=data_root,
         test_mode=True,
-        data_prefix=dict(img='val2017/'),
-        ann_file='annotations/instances_val2017.json',
+        data_prefix=dict(img=val_data_prefix),
+        ann_file=val_ann_file,
         pipeline=test_pipeline,
         batch_shapes_cfg=batch_shapes_cfg))
 
@@ -205,7 +232,7 @@ optim_wrapper = dict(
         type='SGD',
         lr=base_lr,
         momentum=0.937,
-        weight_decay=0.0005,
+        weight_decay=weight_decay,
         nesterov=True,
         batch_size_per_gpu=train_batch_size_per_gpu),
     constructor='YOLOv5OptimizerConstructor')
@@ -214,13 +241,13 @@ default_hooks = dict(
     param_scheduler=dict(
         type='YOLOv5ParamSchedulerHook',
         scheduler_type='linear',
-        lr_factor=0.01,
+        lr_factor=lr_factor,
         max_epochs=max_epochs),
     checkpoint=dict(
         type='CheckpointHook',
         interval=save_epoch_intervals,
         save_best='auto',
-        max_keep_ckpts=3))
+        max_keep_ckpts=max_keep_ckpts))
 
 custom_hooks = [
     dict(
@@ -235,7 +262,7 @@ custom_hooks = [
 val_evaluator = dict(
     type='mmdet.CocoMetric',
     proposal_nums=(100, 1, 10),
-    ann_file=data_root + 'annotations/instances_val2017.json',
+    ann_file=data_root + val_ann_file,
     metric='bbox')
 test_evaluator = val_evaluator
 
