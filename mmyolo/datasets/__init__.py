11fce9895e113e17e021b80b098e4f1f287d83f9
liuyanyi <wolfsonliu@163.com>
2023-02-09 09:59:52 +0800

    update config

--- mmyolo/datasets/__init__.py
+++ mmyolo/datasets/__init__.py
@@ -4,11 +4,9 @@ from .utils import BatchShapePolicy, yolov5_collate
 from .yolov5_coco import YOLOv5CocoDataset
 from .yolov5_crowdhuman import YOLOv5CrowdHumanDataset
 from .yolov5_dota import YOLOv5DOTADataset
-from .yolov5_hrsc import YOLOv5HRSCDataset
 from .yolov5_voc import YOLOv5VOCDataset
 
 __all__ = [
     'YOLOv5CocoDataset', 'YOLOv5VOCDataset', 'BatchShapePolicy',
-    'yolov5_collate', 'YOLOv5CrowdHumanDataset', 'YOLOv5DOTADataset',
-    'YOLOv5HRSCDataset'
+    'yolov5_collate', 'YOLOv5CrowdHumanDataset', 'YOLOv5DOTADataset'
 ]
