e966ce4e600bebff873680547f977b5f159b5d6d
Nioolek <40284075+Nioolek@users.noreply.github.com>
2023-02-09 19:08:21 +0800

    [Improve] Beauty RTMDet config (#531)
    * beauty rtmdet config
    
    * format

--- configs/rtmdet/rtmdet_m_syncbn_fast_8xb32-300e_coco.py
+++ configs/rtmdet/rtmdet_m_syncbn_fast_8xb32-300e_coco.py
@@ -1,8 +1,10 @@
 _base_ = './rtmdet_l_syncbn_fast_8xb32-300e_coco.py'
 
+# ========================modified parameters======================
 deepen_factor = 0.67
 widen_factor = 0.75
 
+# =======================Unmodified in most cases==================
 model = dict(
     backbone=dict(deepen_factor=deepen_factor, widen_factor=widen_factor),
     neck=dict(deepen_factor=deepen_factor, widen_factor=widen_factor),
