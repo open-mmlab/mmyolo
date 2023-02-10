c63c9d95a8a6d588e266280f3b2f1bc2580b081f
aptsunny <36404164+aptsunny@users.noreply.github.com>
2023-02-10 15:59:43 +0800

    fix yolo cfg

--- tests/test_downstream/test_mmrazor.py
+++ tests/test_downstream/test_mmrazor.py
@@ -3,20 +3,19 @@ import copy
 
 import pytest
 from mmcls.models.backbones.base_backbone import BaseBackbone
-from mmengine.config import Config
+
+from mmyolo.testing import get_detector_cfg
 
 
 @pytest.mark.parametrize('cfg_file', [
-    './tests/test_downstream/configs_mmrazor/'
-    'yolov5_s_spos_shufflenetv2_syncbn_8xb16-300e_coco.py',
-    './tests/test_downstream/configs_mmrazor/'
-    'rtmdet_tiny_ofa_lat31_syncbn_16xb16-300e_coco.py',
-    './tests/test_downstream/configs_mmrazor/'
+    'razor/subnets/'
+    'yolov5_s_spos_shufflenetv2_syncbn_8xb16-300e_coco.py', 'razor/subnets/'
+    'rtmdet_tiny_ofa_lat31_syncbn_16xb16-300e_coco.py', 'razor/subnets/'
     'yolov6_l_attentivenas_a6_d12_syncbn_fast_16xb16-300e_coco.py'
 ])
-def test_yolov5_s_spos_forward(cfg_file):
-    config = Config.fromfile(cfg_file)
-    model_cfg = copy.deepcopy(config.model.backbone)
+def test_razor_backbone_forward(cfg_file):
+    model = get_detector_cfg(cfg_file)
+    model_cfg = copy.deepcopy(model.backbone)
     from mmrazor.registry import MODELS
     model = MODELS.build(model_cfg)
     assert isinstance(model, BaseBackbone)
