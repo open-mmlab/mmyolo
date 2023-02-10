593d052294e6523636112a269d78a250b5f450e4
FZY <2788321369@qq.com>
2023-02-10 15:19:14 +0800

    add yolov7 assigner visual

--- projects/assigner_visualization/detectors/__init__.py
+++ projects/assigner_visualization/detectors/__init__.py
@@ -1,5 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from projects.assigner_visualization.detectors.yolo_detector_assigner import \
     YOLODetectorAssigner
+from projects.assigner_visualization.detectors.yolov7_detector_assigner import \
+    YOLOv7DetectorAssigner
 
-__all__ = ['YOLODetectorAssigner']
+__all__ = ['YOLODetectorAssigner', 'YOLOv7DetectorAssigner']
