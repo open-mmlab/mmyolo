7f814f1b21c162b27c5afe90f746acd34b7a4dab
huanghaian <huanghaian@sensetime.com>
2023-02-13 20:05:43 +0800

    update config

--- configs/yolov8/yolov8_l_mask-refine_syncbn_fast_8xb16-500e_coco.py
+++ configs/yolov8/yolov8_l_mask-refine_syncbn_fast_8xb16-500e_coco.py
@@ -1,9 +1,18 @@
 _base_ = './yolov8_m_mask-refine_syncbn_fast_8xb16-500e_coco.py'
 
+# ========================modified parameters======================
 deepen_factor = 1.00
 widen_factor = 1.00
 last_stage_out_channels = 512
-mixup_ratio = 0.15
+
+mixup_prob = 0.15
+copypaste_prob = 0.3
+
+# =======================Unmodified in most cases==================
+img_scale = _base_.img_scale
+pre_transform = _base_.pre_transform
+last_transform = _base_.last_transform
+affine_scale = _base_.affine_scale
 
 model = dict(
     backbone=dict(
@@ -20,18 +29,13 @@ model = dict(
             widen_factor=widen_factor,
             in_channels=[256, 512, last_stage_out_channels])))
 
-pre_transform = _base_.pre_transform
-last_transform = _base_.last_transform
-img_scale = _base_.img_scale
-affine_scale = _base_.affine_scale
-
 mosaic_affine_transform = [
     dict(
         type='Mosaic',
         img_scale=img_scale,
         pad_val=114.0,
         pre_transform=pre_transform),
-    dict(type='YOLOv5CopyPaste', prob=0.3),
+    dict(type='YOLOv5CopyPaste', prob=copypaste_prob),
     dict(
         type='YOLOv5RandomAffine',
         max_rotate_degree=0.0,
@@ -49,7 +53,7 @@ train_pipeline = [
     *pre_transform, *mosaic_affine_transform,
     dict(
         type='YOLOv5MixUp',
-        prob=mixup_ratio,
+        prob=mixup_prob,
         pre_transform=[*pre_transform, *mosaic_affine_transform]),
     *last_transform
 ]
--- configs/yolov8/yolov8_m_mask-refine_syncbn_fast_8xb16-500e_coco.py
+++ configs/yolov8/yolov8_m_mask-refine_syncbn_fast_8xb16-500e_coco.py
@@ -1,13 +1,18 @@
 _base_ = './yolov8_s_mask-refine_syncbn_fast_8xb16-500e_coco.py'
 
+# ========================modified parameters======================
 deepen_factor = 0.67
 widen_factor = 0.75
 last_stage_out_channels = 768
 
 affine_scale = 0.9
-mixup_ratio = 0.1
+mixup_prob = 0.1
+copypaste_prob = 0.1
 
+# ===============================Unmodified in most cases====================
 img_scale = _base_.img_scale
+pre_transform = _base_.pre_transform
+last_transform = _base_.last_transform
 
 model = dict(
     backbone=dict(
@@ -24,16 +29,13 @@ model = dict(
             widen_factor=widen_factor,
             in_channels=[256, 512, last_stage_out_channels])))
 
-pre_transform = _base_.pre_transform
-last_transform = _base_.last_transform
-
 mosaic_affine_transform = [
     dict(
         type='Mosaic',
         img_scale=img_scale,
         pad_val=114.0,
         pre_transform=pre_transform),
-    dict(type='YOLOv5CopyPaste', prob=0.1),
+    dict(type='YOLOv5CopyPaste', prob=copypaste_prob),
     dict(
         type='YOLOv5RandomAffine',
         max_rotate_degree=0.0,
@@ -51,9 +53,29 @@ train_pipeline = [
     *pre_transform, *mosaic_affine_transform,
     dict(
         type='YOLOv5MixUp',
-        prob=mixup_ratio,
+        prob=mixup_prob,
         pre_transform=[*pre_transform, *mosaic_affine_transform]),
     *last_transform
 ]
 
+train_pipeline_stage2 = [
+    *pre_transform,
+    dict(type='YOLOv5KeepRatioResize', scale=img_scale),
+    dict(
+        type='LetterResize',
+        scale=img_scale,
+        allow_scale_up=True,
+        pad_val=dict(img=114.0)),
+    dict(
+        type='YOLOv5RandomAffine',
+        max_rotate_degree=0.0,
+        max_shear_degree=0.0,
+        scaling_ratio_range=(1 - affine_scale, 1 + affine_scale),
+        max_aspect_ratio=_base_.max_aspect_ratio,
+        border_val=(114, 114, 114),
+        min_area_ratio=_base_.min_area_ratio,
+        use_mask_refine=_base_.use_mask2refine), *last_transform
+]
+
 train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
+_base_.custom_hooks[1].switch_pipeline = train_pipeline_stage2
