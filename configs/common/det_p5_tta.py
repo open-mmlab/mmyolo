0ced46623b90a9348f01141345baf2aba039c422
huanghaian <huanghaian@sensetime.com>
2023-02-14 17:01:36 +0800

    support TTA

---
+++ configs/common/det_p5_tta.py
@@ -0,0 +1,30 @@
+tta_model = dict(
+    type='mmdet.DetTTAModel',
+    tta_cfg=dict(nms=dict(type='nms', iou_threshold=0.65), max_per_img=100))
+
+img_scales = [(640, 640), (320, 320), (960, 960)]
+
+tta_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=dict(backend='disk')),
+    dict(
+        type='TestTimeAug',
+        transforms=[[[
+            dict(type='YOLOv5KeepRatioResize', scale=s),
+            dict(
+                type='LetterResize',
+                scale=s,
+                allow_scale_up=False,
+                pad_val=dict(img=114))
+        ] for s in img_scales],
+                    [
+                        dict(type='mmdet.RandomFlip', prob=1.),
+                        dict(type='mmdet.RandomFlip', prob=0.)
+                    ], [dict(type='mmdet.LoadAnnotations', with_bbox=True)],
+                    [
+                        dict(
+                            type='mmdet.PackDetInputs',
+                            meta_keys=('img_id', 'img_path', 'ori_shape',
+                                       'img_shape', 'scale_factor',
+                                       'pad_param', 'flip', 'flip_direction'))
+                    ]])
+]
