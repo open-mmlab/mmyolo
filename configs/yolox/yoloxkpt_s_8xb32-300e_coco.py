4c5a585510c0cc0d824a00ddbf37b478dc0c187e
houbowei <houbowei@gddi.com.cn>
2023-02-24 19:22:22 +0800

    delete useless configs

--- configs/yolox/yoloxkpt_s_8xb32-300e_coco.py
+++ configs/yolox/yoloxkpt_s_8xb32-300e_coco.py
@@ -1,4 +1,4 @@
-_base_ = '../_base_/default_runtime.py'
+_base_ = './default_yolopose.py'
 dataset_info = '../_base_/datasets/coco.py'
 
 data_root = 'data/coco/'
@@ -9,27 +9,29 @@ img_scale = (640, 640)  # width, height
 deepen_factor = 0.33
 widen_factor = 0.5
 
-save_epoch_intervals = 1
-train_batch_size_per_gpu = 8
+save_epoch_intervals = 10
+train_batch_size_per_gpu = 128
 # NOTE: for debugging set to 0
-train_num_workers = 8
+train_num_workers = 4
 val_batch_size_per_gpu = 1
-# NOTE: for debugging set to 0
-val_num_workers = 8
+val_num_workers = 4
 
-max_epochs = 100  # NOTE: for debug
-num_last_epochs = 15
+max_epochs = 100
+num_last_epochs = 20
+batch_augments_interval = 1
 
 # model settings
 model = dict(
     type='YOLODetector',
-    init_cfg=dict(
-        type='Kaiming',
-        layer='Conv2d',
-        a=2.23606797749979,  # math.sqrt(5)
-        distribution='uniform',
-        mode='fan_in',
-        nonlinearity='leaky_relu'),
+    init_cfg=[
+        dict(
+            type='Kaiming',
+            layer='Conv2d',
+            a=2.23606797749979,  # math.sqrt(5)
+            distribution='uniform',
+            mode='fan_in',
+            nonlinearity='leaky_relu')
+        ],
     # TODO: Waiting for mmengine support
     use_syncbn=False,
     data_preprocessor=dict(
@@ -37,11 +39,12 @@ model = dict(
         pad_size_divisor=32,
         batch_augments=[
             dict(
-                type='mmdet.BatchSyncRandomResize',
+                type='YOLOXPoseBatchSyncRandomResize',
                 random_size_range=(480, 800),
                 size_divisor=32,
-                interval=10)
-        ]),
+                interval=batch_augments_interval)
+        ]
+        ),
     backbone=dict(
         type='YOLOXCSPDarknet',
         deepen_factor=deepen_factor,
@@ -94,7 +97,7 @@ model = dict(
             loss_weight=1.0),
         loss_bbox_aux=dict(
             type='mmdet.L1Loss', reduction='sum', loss_weight=1.0),
-        loss_kpt=dict(type='mmpose.SmoothL1Loss')),
+        loss_kpt=dict(type='OksLoss', loss_type='oks_yolox', dataset_info=dataset_info, loss_weight=70)),
     train_cfg=dict(
         assigner=dict(
             type='mmdet.SimOTAAssigner',
@@ -102,8 +105,8 @@ model = dict(
             iou_calculator=dict(type='mmdet.BboxOverlaps2D'))),
     test_cfg=dict(
         yolox_style=True,  # better
-        multi_label=True,  # 40.5 -> 40.7
-        score_thr=0.001,
+        multi_label=False,  # 40.5 -> 40.7
+        score_thr=0.01,
         max_per_img=300,
         nms=dict(type='nms', iou_threshold=0.65)))
 
@@ -123,6 +126,7 @@ train_pipeline_stage1 = [
     dict(
         # type='mmdet.RandomAffine',
         type='YOLOPoseRandomAffine',
+        max_rotate_degree=10.0,
         scaling_ratio_range=(0.75, 1),
         max_translate_ratio=0.0,
         max_shear_degree=2.0,
@@ -172,27 +176,27 @@ train_pipeline_stage2 = [
 train_dataloader = dict(
     batch_size=train_batch_size_per_gpu,
     num_workers=train_num_workers,
-    persistent_workers=False,  # NOTE: for debugging
+    persistent_workers=True,  # NOTE: for debugging
     pin_memory=False,  # NOTE: for debugging
     sampler=dict(type='DefaultSampler', shuffle=True),
+    # collate_fn=dict(type='yolov5_collate'),
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
         metainfo=dataset_info,
-        ann_file='annotations/person_keypoints_train2017_6280.json',
+        ann_file='annotations/person_keypoints_train2017.json',
         data_prefix=dict(img='train2017/'),
-        filter_cfg=dict(filter_empty_gt=False, min_size=32),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
         pipeline=train_pipeline_stage1))
 
 test_pipeline = [
     *pre_transform,
-    # dict(type='LoadImageFromFile', file_client_args=_base_.file_client_args),
     dict(type='YOLOPoseResize', scale=img_scale, keep_ratio=True),
     dict(
         type='mmdet.Pad',
         pad_to_square=True,
         pad_val=dict(img=(114.0, 114.0, 114.0))),
-     dict(
+    dict(
         type='YOLOPoseFilterAnnotations',
         min_gt_bbox_wh=(1, 1),
         keep_empty=False,
@@ -203,12 +207,12 @@ test_pipeline = [
         meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
                    'scale_factor'))
 ]
-# test_pipeline = train_pipeline_stage2
 
 val_dataloader = dict(
     batch_size=val_batch_size_per_gpu,
     num_workers=val_num_workers,
-    persistent_workers=False,  # NOTE: for debugging
+    persistent_workers=True,  # NOTE: for debugging
+    # collate_fn=dict(type='yolov5_collate'),
     pin_memory=False,  # NOTE: for debugging
     drop_last=False,
     sampler=dict(type='DefaultSampler', shuffle=False),
@@ -216,7 +220,7 @@ val_dataloader = dict(
         type=dataset_type,
         data_root=data_root,
         metainfo=dataset_info,
-        ann_file='annotations/person_keypoints_val2017_mini.json',
+        ann_file='annotations/person_keypoints_val2017.json',
         data_prefix=dict(img='val2017/'),
         test_mode=True,
         pipeline=test_pipeline,
@@ -228,11 +232,13 @@ val_evaluator = [
     dict(
         type='mmdet.CocoMetric',
         proposal_nums=(100, 1, 10),
-        ann_file=data_root + 'annotations/person_keypoints_val2017_mini.json',
+        ann_file=data_root + 'annotations/person_keypoints_val2017.json',
         metric=['bbox']),
     dict(
         type='CocoMetric',
-        ann_file=data_root + 'annotations/person_keypoints_val2017_mini.json')
+        ann_file=data_root + 'annotations/person_keypoints_val2017.json',
+        score_mode='bbox',
+        nms_mode='none')
 ]
 
 test_evaluator = val_evaluator
@@ -240,14 +246,13 @@ test_evaluator = val_evaluator
 # optimizer
 # default 8 gpu
 # NOTE: clip grad is necessary for training.
-base_lr = 0.01
+base_lr = 0.004
 optim_wrapper = dict(
+    _delete_=True,
     type='OptimWrapper',
-    optimizer=dict(
-        type='SGD', lr=base_lr, momentum=0.9, weight_decay=5e-4,
-        nesterov=True),
-    paramwise_cfg=dict(norm_decay_mult=0., bias_decay_mult=0.),
-    clip_grad=dict(max_norm=35, norm_type=2))
+    optimizer=dict(type='AdamW', lr=base_lr, weight_decay=0.05),
+    paramwise_cfg=dict(
+        norm_decay_mult=0, bias_decay_mult=0, bypass_duplicate=True))
 
 # learning rate
 param_scheduler = [
@@ -279,11 +284,7 @@ param_scheduler = [
     )
 ]
 
-default_hooks = dict(
-    checkpoint=dict(
-        type='CheckpointHook', interval=1, max_keep_ckpts=3, save_best=['auto'],
-        greater_keys=['coco/AP', 'coco/bbox_mAP'])
-    )
+vis_backends = [dict(type='WandbVisBackend')]
 
 custom_hooks = [
     dict(
@@ -295,10 +296,11 @@ custom_hooks = [
     dict(
         type='EMAHook',
         ema_type='ExpMomentumEMA',
-        momentum=0.0001,
+        momentum=0.0002,
         update_buffers=True,
         strict_load=False,
-        priority=49)
+        priority=49),
+    dict(type="WandbGradVisHook")
 ]
 
 train_cfg = dict(
@@ -307,36 +309,7 @@ train_cfg = dict(
     val_interval=save_epoch_intervals,
     dynamic_intervals=[(max_epochs - num_last_epochs, 1)])
 
-auto_scale_lr = dict(base_batch_size=64)
+auto_scale_lr = dict(base_batch_size=2*train_batch_size_per_gpu)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
-
-# 17 keypoints
-keypoint_colors = [(255, 0, 0), (255, 85, 0), (255, 170, 0), (255, 0, 85),
-                   (255, 0, 170), (0, 255, 0), (85, 255, 0), (170, 255, 0),
-                   (0, 255, 85), (0, 255, 170), (0, 0, 255), (85, 0, 255),
-                   (170, 0, 255), (0, 85, 255), (0, 170, 255), (255, 255, 0),
-                   (255, 255, 85)]
-
-skeleton_links = [(0, 1), (0, 2), (1, 3), (2, 4), (0, 5), (0, 6), (5, 7),
-                  (7, 9), (6, 8), (8, 10), (5, 6), (5, 11), (6, 12), (11, 12),
-                  (11, 13), (13, 15), (12, 14), (14, 16)]
-
-# 18 links
-skeleton_links_colors = [
-    (255, 0, 0), (255, 85, 0), (255, 170, 0), (255, 0, 85), (255, 0, 170),
-    (0, 255, 0), (85, 255, 0), (170, 255, 0), (0, 255, 85), (0, 255, 170),
-    (0, 0, 255), (85, 0, 255), (170, 0, 255), (0, 85, 255), (0, 170, 255),
-    (255, 255, 0), (255, 255, 85), (255, 0, 255)
-]
-
-vis_backends = [dict(type='LocalVisBackend'), dict(type='WandbVisBackend')]
-visualizer = dict(
-    # type='mmdet.DetLocalVisualizer',
-    type='mmpose.PoseLocalVisualizer',
-    vis_backends=vis_backends,
-    kpt_color=keypoint_colors,
-    skeleton=skeleton_links,
-    link_color=skeleton_links_colors,
-    name='visualizer')
-seed=0
+# load_from="mmyoloxs.pt"
--- download_coco.sh
+++
@@ -1,9 +0,0 @@
-# Dataset origin
-cp -r /dataset/chenjn/dataset/coco ./
-# tar -xvf coco/annotations.tar.gz -C coco/
-ln -s /dataset/houbowei/coco/annotations ./coco/annotations
-tar -xvf coco/train2017.tar.gz -C coco/
-tar -xvf coco/val2017.tar.gz -C coco/
-rm coco/*.gz
-mkdir data
-mv coco data/coco
