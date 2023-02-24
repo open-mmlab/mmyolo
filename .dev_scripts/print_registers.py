5804e8929581c8d3864e807c83e49a40d081f9fd
tang576225574 <576225574@qq.com>
2023-02-24 19:51:50 +0800

    tang v1 finished

--- .dev_scripts/print_registers.py
+++ .dev_scripts/print_registers.py
@@ -9,8 +9,7 @@ import pkgutil
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from mmengine.utils import mkdir_or_exist, scandir
-from mmengine.registry import Registry
+from multiprocessing import Pool
 
 # host_addr = 'https://gitee.com/open-mmlab'
 host_addr = 'https://github.com/open-mmlab'
@@ -22,6 +21,22 @@ proxy_names = {
 }
 
 
+def capitalize(repo_name):
+    lower = repo_name.lower()
+    if lower == 'mmcv':
+        return repo_name.upper()
+    elif lower.startswith('mm'):
+        return 'MM' + repo_name[2:]
+    return repo_name.capitalize()
+
+
+def mkdir_or_exist(dir_name, mode=0o777):
+    if dir_name == '':
+        return
+    dir_name = osp.expanduser(dir_name)
+    os.makedirs(dir_name, mode=mode, exist_ok=True)
+
+
 def parse_repo_name(repo_name):
     proxy_names_rev = dict(zip(proxy_names.values(), proxy_names.keys()))
     repo_name = proxy_names.get(repo_name, repo_name)
@@ -30,6 +45,7 @@ def parse_repo_name(repo_name):
 
 
 def git_pull_branch(repo_name, branch_name='', pulldir='.'):
+    mkdir_or_exist(pulldir)
     exec_str = f'cd {pulldir};git init;git pull '
     exec_str += f'{host_addr}/{repo_name}.git'
     if branch_name:
@@ -41,7 +57,7 @@ def git_pull_branch(repo_name, branch_name='', pulldir='.'):
 
 
 def load_modules_from_dir(module_name, module_root, throw_error=False):
-    print('loading the modules...')
+    print(f'loading the {module_name} modules...')
     # # install the dependencies
     # if osp.exists(osp.join(pkg_dir, 'requirements.txt')):
     #     os.system('pip install -r requirements.txt')
@@ -76,6 +92,9 @@ def load_modules_from_dir(module_name, module_root, throw_error=False):
             assert (_name not in error_dict), \
                 f'duplicate error name was found: {_name}'
             error_dict[_name] = _error_msg
+    for module in module_list:
+        assert module.__file__.startswith(module_root), \
+            f'the importing path of package was wrong: {module.__file__}'
     print('modules were loaded...')
     return module_list, error_dict
 
@@ -83,6 +102,10 @@ def load_modules_from_dir(module_name, module_root, throw_error=False):
 def get_registries_from_modules(module_list):
     registries = {}
     objects_set = set()
+    # import the Registry class,
+    # import at the beginning is not allowed
+    # because it is not the temp package
+    from mmengine.registry import Registry
     # only get the specific registries in module list
     print('getting registries...')
     for module in module_list:
@@ -104,7 +127,7 @@ def get_registries_from_modules(module_list):
     return registries
 
 
-def get_pyfiles_from_dir(root):
+def get_scripts_from_dir(root):
 
     def _recurse(_dict, _chain):
         if len(_chain) <= 1:
@@ -116,11 +139,14 @@ def get_pyfiles_from_dir(root):
         _recurse(_dict[_key], _chain)
 
     # find all scripts in the root directory. (not just ('.py', '.sh'))
-    pyfiles = {}
-    if osp.isdir(root):
-        for pyfile in scandir(root, recursive=True):
-            _recurse(pyfiles, Path(pyfile).parts)
-    return pyfiles
+    # can not use the scandir function in mmengine to scan the dir,
+    # because mmengine import is not allowed before git pull
+    scripts = {}
+    for _subroot, _dirs, _files in os.walk(root):
+        for _file in _files:
+            _script = osp.join(osp.relpath(_subroot, root), _file)
+            _recurse(scripts, Path(_script).parts)
+    return scripts
 
 
 def print_tree(print_dict):
@@ -190,7 +216,8 @@ def registries_to_html(registries, title=''):
         if len(registry_strings) > max_size_per_cell:
             multi_parts = True
         for cell_idx, registry_cell in enumerate(
-                divide_list_into_groups(registry_strings, max_size_per_cell)):
+                divide_list_into_groups(registry_strings,
+                                        max_size_per_cell)):
             registry_str = ''.join(registry_cell.tolist())
             registry_str = f'<ul>{registry_str}</ul>'
             table_data_multi_parts.append([
@@ -203,7 +230,9 @@ def registries_to_html(registries, title=''):
         table_data = list(zip(*table_data.tolist()))
         html += dataframe_to_html(
             pd.DataFrame([table_data[1]], columns=table_data[0]))
-    html = add_html_title(html, title)
+    if html:
+        html = f'<div align=\'center\'><b>{title}</b></div>\n{html}'
+        html = f'<details open>{html}</details>\n'
     return html
 
 
@@ -221,7 +250,7 @@ def tools_to_html(tools_dict, repo_name='', tools_name=''):
                 _recurse(_v, f'{_connector}/{_k}', _result)
 
     table_data = {}
-    title = f'{repo_name.capitalize()} {tools_name.capitalize()}'
+    title = f'{capitalize(repo_name)} {tools_name.title()}'
     _recurse(tools_dict, tools_name, table_data)
     return registries_to_html(table_data, title)
 
@@ -231,7 +260,7 @@ def dataframe_to_html(dataframe):
     styler = styler.hide(axis='index')
     styler = styler.format(na_rep='-')
     styler = styler.set_properties(**{
-        'text-align': 'center',
+        'text-align': 'left',
         'align': 'center',
         'vertical-align': 'top'
     })
@@ -242,38 +271,72 @@ def dataframe_to_html(dataframe):
         'align:center;text-align:center;vertical-align:bottom'
     }])
     html = styler.to_html()
-    html = f'<div align=\'center\'>{html}</div><br><br>'
+    html = f'<div align=\'center\'>\n{html}</div>'
     return html
 
 
-def add_html_title(html, title):
-    html = f'<div align=\'center\'><b>{title}</b></div>\n{html}'
-    html = f'<details open>{html}</details>'
-    return html
+def generate_markdown_by_repository(repo_name,
+                                    module_name,
+                                    pulldir,
+                                    throw_error=False):
+    # add the pull dir to the system path so that it can be found
+    if pulldir not in sys.path:
+        sys.path.insert(0, pulldir)
+    module_list, error_dict = load_modules_from_dir(
+        module_name, pulldir, throw_error=throw_error)
+    registries_tree = get_registries_from_modules(module_list)
+    if error_dict:
+        error_dict_name = 'error_modules'
+        assert (error_dict_name not in registries_tree), \
+            f'duplicate module name was found: {error_dict_name}'
+        registries_tree.update({error_dict_name: error_dict})
+    # get the tools files
+    for tools_name in tools_list:
+        assert (tools_name not in registries_tree), \
+            f'duplicate tools name was found: {tools_name}'
+        tools_tree = osp.join(pulldir, tools_name)
+        tools_tree = get_scripts_from_dir(tools_tree)
+        registries_tree.update({tools_name: tools_tree})
+    # print_tree(registries_tree)
+    # get registries markdown string
+    markdown_str = registries_to_html(
+        registries_tree.get(module_name, {}),
+        title=f'{capitalize(repo_name)} Module Components')
+    # get tools markdown string
+    for tools_name in tools_list:
+        markdown_str += tools_to_html(
+            registries_tree.get(tools_name, {}),
+            repo_name=repo_name,
+            tools_name=tools_name)
+    markdown_str = f'\n\n## {capitalize(repo_name)}\n{markdown_str}'
+    # remove the pull dir from system path
+    if pulldir in sys.path:
+        sys.path.remove(pulldir)
+    return markdown_str
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='print registries in a repository')
+        description='print registries in mmlab repositories')
     parser.add_argument(
-        'repository', type=str,
-        help='git repository name in OpenMMLab')
+        'repositories',
+        nargs='+',
+        type=str,
+        help='git repositories name in OpenMMLab')
     parser.add_argument(
-        '-b', '--branch', type=str,
-        help='the branch name of git repository')
+        '-b',
+        '--branches',
+        nargs='+',
+        type=str,
+        help='the branch names of git repositories, the length of branches '
+        'must be same as the length of repositories')
     parser.add_argument(
-        '-o', '--out', type=str, default='.',
-        help='output path of the result')
+        '-o', '--out', type=str, default='.', help='output path of the file')
     parser.add_argument(
         '--throw-error',
         action='store_true',
         default=False,
         help='whether to throw error when trying to import modules')
-    parser.add_argument(
-        '--without-tools',
-        action='store_true',
-        default=False,
-        help='whether to print the scripts of tools directory')
     args = parser.parse_args()
     return args
 
@@ -281,53 +344,45 @@ def parse_args():
 # TODO: Refine
 def main():
     args = parse_args()
-    repo_name, module_name = parse_repo_name(args.repository)
-    assert (repo_name not in ['mmengine', 'mmcv']), \
-        'mmengine or mmcv is not supported temporarily for querying'
-    with tempfile.TemporaryDirectory() as tmpdir:
-        # get the registries
-        git_pull_branch(
-            repo_name=repo_name, branch_name=args.branch, pulldir=tmpdir)
-        if tmpdir not in sys.path:
-            sys.path.insert(0, tmpdir)
-        module_list, error_dict = load_modules_from_dir(
-            module_name, tmpdir, throw_error=args.throw_error)
-        registries_tree = get_registries_from_modules(module_list)
-        if error_dict:
-            error_dict_name = 'error_modules'
-            assert (error_dict_name not in registries_tree), \
-                f'duplicate module name was found: {error_dict_name}'
-            registries_tree.update({error_dict_name: error_dict})
-        # get the tools files
-        if not args.without_tools:
-            for tools_name in tools_list:
-                assert (tools_name not in registries_tree), \
-                    f'duplicate tools name was found: {tools_name}'
-                tools_tree = osp.join(tmpdir, tools_name)
-                tools_tree = get_pyfiles_from_dir(tools_tree)
-                registries_tree.update({tools_name: tools_tree})
-        print_tree(registries_tree)
-        if args.out:
-            # get registries markdown string
-            mkdir_or_exist(args.out)
-            markdown_str = registries_to_html(
-                registries_tree.get(module_name, {}),
-                title=f'{repo_name.capitalize()} Module Components')
-
-            # get tools markdown string
-            if not args.without_tools:
-                for tools_name in tools_list:
-                    markdown_str += tools_to_html(
-                        registries_tree.get(tools_name, {}),
-                        repo_name=repo_name,
-                        tools_name=tools_name)
-
-            # save the file
-            save_path = osp.join(args.out,
-                                 f'registries_info_{module_name.lower()}.md')
-            with open(save_path, 'w', encoding='utf-8') as fw:
-                fw.write(markdown_str)
-            print(f'saved registries to the path: {save_path}')
+    repositories = args.repositories
+    branches = args.branches
+    assert isinstance(repositories, list), \
+        'Type of repositories must be list'
+    if branches is None:
+        branches = [None] * len(repositories)
+    assert isinstance(branches, list) and \
+        len(branches) == len(repositories), \
+            'The length of branches must be same as that of repositories'
+    assert isinstance(args.out, str), \
+        'The type of output path must be string'
+    # save path of file
+    mkdir_or_exist(args.out)
+    save_path = osp.join(args.out, f'registries_info.md')
+    with open(save_path, 'w', encoding='utf-8') as fw:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            # multi process init
+            pool = Pool(processes=len(repositories))
+            multi_proc_input_list = []
+            multi_proc_output_list = []
+            # get the git repositories
+            for branch, repository in zip(branches, repositories):
+                repo_name, module_name = parse_repo_name(repository)
+                pulldir = osp.join(tmpdir, f'tmp_{repo_name}')
+                git_pull_branch(
+                    repo_name=repo_name, branch_name=branch, pulldir=pulldir)
+                multi_proc_input_list.append(
+                    (repo_name, module_name, pulldir, args.throw_error))
+            print('starting the multi process to get the registries')
+            for multi_proc_input in multi_proc_input_list:
+                multi_proc_output_list.append(
+                    pool.apply_async(generate_markdown_by_repository,
+                                     multi_proc_input))
+            pool.close()
+            pool.join()
+            for multi_proc_output in multi_proc_output_list:
+                markdown_str = multi_proc_output.get()
+                fw.write(f'{markdown_str}\n')
+    print(f'saved registries to the path: {save_path}')
 
 
 if __name__ == '__main__':
