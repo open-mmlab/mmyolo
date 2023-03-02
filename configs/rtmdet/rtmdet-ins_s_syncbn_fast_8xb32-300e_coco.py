da9c7d5d50c5fa84e6efbcb9d626a8e183ff2c49
Nioolek <379319054@qq.com>
2023-03-02 10:19:10 +0800

    beauty config

--- configs/rtmdet/rtmdet-ins_s_syncbn_fast_8xb32-300e_coco.py
+++ configs/rtmdet/rtmdet-ins_s_syncbn_fast_8xb32-300e_coco.py
@@ -1,36 +1,31 @@
 _base_ = './rtmdet_s_syncbn_fast_8xb32-300e_coco.py'
 
+widen_factor = 0.5
+
 model = dict(
     bbox_head=dict(
         type='RTMDetInsSepBNHead',
         head_module=dict(
-            # rewrite head_module cfg
-            _delete_=True,
             type='RTMDetInsSepBNHeadModule',
-            num_classes=_base_.num_classes,
-            in_channels=256,
-            feat_channels=256,
-            stacked_convs=2,
-            norm_cfg=dict(type='BN'),
-            act_cfg=dict(type='SiLU', inplace=True),
-            share_conv=True,
-            pred_kernel_size=1,
             use_sigmoid_cls=True,
-            featmap_strides=[8, 16, 32],
-            widen_factor=0.5),
+            widen_factor=widen_factor),
         loss_mask=dict(
             type='mmdet.DiceLoss', loss_weight=2.0, eps=5e-6,
             reduction='mean')),
     test_cfg=dict(
-        # rewrite test_cfg
-        _delete_=True,
-        multi_label=False,
+        multi_label=True,
         nms_pre=1000,
         min_bbox_size=0,
         score_thr=0.05,
         nms=dict(type='nms', iou_threshold=0.6),
         max_per_img=100,
         mask_thr_binary=0.5))
+
+_base_.test_pipeline[-2] = dict(
+    type='LoadAnnotations', with_bbox=True, with_mask=True, _scope_='mmdet')
+
+val_dataloader = dict(dataset=dict(pipeline=_base_.test_pipeline))
+test_dataloader = val_dataloader
 
 val_evaluator = dict(metric=['bbox', 'segm'])
 test_evaluator = val_evaluator
