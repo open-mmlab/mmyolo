bcdc6da47054d1892c04e5ce5bee0a47f2168103
aptsunny <36404164+aptsunny@users.noreply.github.com>
2023-03-02 16:20:40 +0800

    fix test ci

--- tests/test_downstream/test_mmrazor.py
+++ tests/test_downstream/test_mmrazor.py
@@ -11,7 +11,7 @@ from mmyolo.testing import get_detector_cfg
     'razor/subnets/'
     'yolov5_s_spos_shufflenetv2_syncbn_8xb16-300e_coco.py', 'razor/subnets/'
     'rtmdet_tiny_ofa_lat31_syncbn_16xb16-300e_coco.py', 'razor/subnets/'
-    'yolov6_l_attentivenas_a6_d12_syncbn_fast_16xb16-300e_coco.py'
+    'yolov6_l_attentivenas_a6_d12_syncbn_fast_8xb32-300e_coco.py'
 ])
 def test_razor_backbone_init(cfg_file):
     model = get_detector_cfg(cfg_file)
