b6b5441b736f446aea44bf10b1610353f70baeb1
Nioolek <379319054@qq.com>
2023-03-07 10:59:59 +0800

    add ut

--- tests/test_models/test_dense_heads/test_yolov8_head.py
+++ tests/test_models/test_dense_heads/test_yolov8_head.py
@@ -5,7 +5,7 @@ import torch
 from mmengine import ConfigDict
 from mmengine.config import Config
 
-from mmyolo.models import YOLOv8Head
+from mmyolo.models import YOLOv8Head, YOLOv8InsHead
 from mmyolo.utils import register_all_modules
 
 register_all_modules()
@@ -159,3 +159,71 @@ class TestYOLOv8Head(TestCase):
                            'box loss should be non-zero')
         self.assertGreater(onegt_loss_dfl.item(), 0,
                            'obj loss should be non-zero')
+
+
+class TestYOLOv8InsHead(TestCase):
+
+    def setUp(self):
+        self.head_module = dict(
+            type='YOLOv8InsHeadModule',
+            num_classes=4,
+            in_channels=[32, 64, 128],
+            featmap_strides=[8, 16, 32],
+            protos_channels=32,
+            masks_channels=32)
+
+    def test_init_weights(self):
+        head = YOLOv8InsHead(head_module=self.head_module)
+        head.head_module.init_weights()
+
+    def test_predict_by_feat(self):
+        s = 256
+        img_metas = [{
+            'img_shape': (s, s, 3),
+            'ori_shape': (s, s, 3),
+            'scale_factor': (1.0, 1.0),
+        }]
+        test_cfg = Config(
+            dict(
+                multi_label=True,
+                nms_pre=30000,
+                min_bbox_size=0,
+                score_thr=0.001,
+                nms=dict(type='nms', iou_threshold=0.7),
+                max_per_img=300,
+                mask_thr_binary=0.5))
+
+        head = YOLOv8InsHead(head_module=self.head_module, test_cfg=test_cfg)
+        head.eval()
+
+        feat = []
+        for i in range(len(self.head_module['in_channels'])):
+            in_channel = self.head_module['in_channels'][i]
+            feat_size = self.head_module['featmap_strides'][i]
+            feat.append(
+                torch.rand(1, in_channel, s // feat_size, s // feat_size))
+
+        cls_scores, bbox_preds, mask_coefficients, mask_protos = head.forward(
+            feat)
+        head.predict_by_feat(
+            cls_scores,
+            bbox_preds,
+            mask_coefficients,
+            mask_protos,
+            None,
+            img_metas,
+            cfg=test_cfg,
+            rescale=True,
+            with_nms=True)
+
+        with self.assertRaises(AssertionError):
+            head.predict_by_feat(
+                cls_scores,
+                bbox_preds,
+                mask_coefficients,
+                mask_protos,
+                None,
+                img_metas,
+                cfg=test_cfg,
+                rescale=True,
+                with_nms=False)
