9c502ee7fc96f38bdda5c0b266de1a96a06ce6c5
yechenzhi@kuaishou.com <136920488@qq.com>
2023-03-25 10:53:53 +0800

    add models

--- mmyolo/models/data_preprocessors/__init__.py
+++ mmyolo/models/data_preprocessors/__init__.py
@@ -1,10 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .data_preprocessor import (PPYOLOEBatchRandomResize,
+from .data_preprocessor import (PoseBatchSyncRandomResize,
+                                PPYOLOEBatchRandomResize,
                                 PPYOLOEDetDataPreprocessor,
                                 YOLOv5DetDataPreprocessor,
                                 YOLOXBatchSyncRandomResize)
 
 __all__ = [
     'YOLOv5DetDataPreprocessor', 'PPYOLOEDetDataPreprocessor',
-    'PPYOLOEBatchRandomResize', 'YOLOXBatchSyncRandomResize'
+    'PPYOLOEBatchRandomResize', 'YOLOXBatchSyncRandomResize',
+    'PoseBatchSyncRandomResize'
 ]
--- mmyolo/models/data_preprocessors/data_preprocessor.py
+++ mmyolo/models/data_preprocessors/data_preprocessor.py
@@ -8,6 +8,7 @@ from mmdet.models import BatchSyncRandomResize
 from mmdet.models.data_preprocessors import DetDataPreprocessor
 from mmengine import MessageHub, is_list_of
 from mmengine.structures import BaseDataElement
+from mmpose.structures import PoseDataSample
 from torch import Tensor
 
 from mmyolo.registry import MODELS
@@ -300,3 +301,29 @@ class PPYOLOEBatchRandomResize(BatchSyncRandomResize):
         else:
             interp_mode = None
         return input_size, interp_mode
+
+
+@MODELS.register_module()
+class PoseBatchSyncRandomResize(BatchSyncRandomResize):
+    """Batch random resize which synchronizes the random size across ranks.
+
+    This transform is similar to `mmdet.BatchSyncRandomResize`, but it also
+    rescales the keypoints coordinates simultaneously.
+    """
+
+    def forward(
+        self, inputs: Tensor, data_samples: List[PoseDataSample]
+    ) -> Tuple[Tensor, List[PoseDataSample]]:
+
+        inputs = inputs.float()
+        h, w = inputs.shape[-2:]
+        if self._input_size is None:
+            self._input_size = (h, w)
+        scale_y = self._input_size[0] / h
+        scale_x = self._input_size[1] / w
+        if scale_x != 1 or scale_y != 1:
+            for data_sample in data_samples:
+                data_sample.gt_instances.keypoints[..., 0] *= scale_x
+                data_sample.gt_instances.keypoints[..., 1] *= scale_y
+
+        return super().forward(inputs, data_samples)
