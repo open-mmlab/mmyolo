7441cb9b7966574a5d19e89bed5f6361f067e9a8
yechenzhi@kuaishou.com <136920488@qq.com>
2023-03-25 12:24:03 +0800

    re commit

--- mmyolo/datasets/transforms/__init__.py
+++ mmyolo/datasets/transforms/__init__.py
@@ -1,15 +1,3 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .mix_img_transforms import Mosaic, Mosaic9, YOLOv5MixUp, YOLOXMixUp
-from .transforms import (
-    FilterDetPoseAnnotations, LetterResize, LoadAnnotations, PackDetPoseInputs,
-    PoseToDetConverter, PPYOLOERandomCrop, PPYOLOERandomDistort,
-    RegularizeRotatedBox, RemoveDataElement, YOLOv5CopyPaste,
-    YOLOv5HSVRandomAug, YOLOv5KeepRatioResize, YOLOv5RandomAffine)
-
-__all__ = [
-    'YOLOv5KeepRatioResize', 'LetterResize', 'Mosaic', 'YOLOXMixUp',
-    'YOLOv5MixUp', 'YOLOv5HSVRandomAug', 'LoadAnnotations',
-    'YOLOv5RandomAffine', 'PPYOLOERandomDistort', 'PPYOLOERandomCrop',
-    'Mosaic9', 'YOLOv5CopyPaste', 'RemoveDataElement', 'RegularizeRotatedBox',
-    'PoseToDetConverter', 'PackDetPoseInputs', 'FilterDetPoseAnnotations'
-]
+from .mix_img_transforms import *  # noqa
+from .transforms import *  # noqa
