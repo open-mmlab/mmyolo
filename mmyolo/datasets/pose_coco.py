3c7e033122280d8415c88b6ca74a7d66fea327ab
yechenzhi@kuaishou.com <136920488@qq.com>
2023-03-25 11:24:35 +0800

    re commit __init__

---
+++ mmyolo/datasets/pose_coco.py
@@ -0,0 +1,17 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from typing import Any
+
+from mmengine.dataset import force_full_init
+from mmpose.datasets import CocoDataset as MMPoseCocoDataset
+
+from ..registry import DATASETS
+
+
+@DATASETS.register_module()
+class CocoPoseDataset(MMPoseCocoDataset):
+
+    @force_full_init
+    def prepare_data(self, idx) -> Any:
+        data_info = self.get_data_info(idx)
+        data_info['dataset'] = self
+        return self.pipeline(data_info)
