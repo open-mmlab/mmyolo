b73c07f6a45adec58517ed784be26081ee13dfe9
Lingrui Gu <lingrui.gu@nanovision.com.cn>
2023-04-18 11:32:51 +0800

    del temp neck

--- mmyolo/models/necks/__init__.py
+++ mmyolo/models/necks/__init__.py
@@ -2,7 +2,7 @@
 from .base_yolo_neck import BaseYOLONeck
 from .cspnext_pafpn import CSPNeXtPAFPN
 from .ppyoloe_csppan import PPYOLOECSPPAFPN
-from .yolov5_pafpn import TempCLIPdownsampleneck, YOLOv5PAFPN
+from .yolov5_pafpn import YOLOv5PAFPN
 from .yolov6_pafpn import YOLOv6CSPRepPAFPN, YOLOv6RepPAFPN
 from .yolov7_pafpn import YOLOv7PAFPN
 from .yolov8_pafpn import YOLOv8PAFPN
@@ -11,5 +11,5 @@ from .yolox_pafpn import YOLOXPAFPN
 __all__ = [
     'YOLOv5PAFPN', 'BaseYOLONeck', 'YOLOv6RepPAFPN', 'YOLOXPAFPN',
     'CSPNeXtPAFPN', 'YOLOv7PAFPN', 'PPYOLOECSPPAFPN', 'YOLOv6CSPRepPAFPN',
-    'YOLOv8PAFPN', 'TempCLIPdownsampleneck'
+    'YOLOv8PAFPN'
 ]
